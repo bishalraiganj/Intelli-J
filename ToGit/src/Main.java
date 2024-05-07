3867c8b2e17b22ba11ae8c5ae34929a4a603bbda
bishalraiganj <bishaladhikaryreal@gmail.com>
2024-05-08 01:33:55 +0530

    try 3

--- ToGit/src/Main.java
+++ ToGit/src/Main.java
@@ -1,8 +1,57 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
 
-            //pushing it to Git3
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        printList(new ArrayList<LPAStudent> ()); //Same container as the methods parameter reference type, since List generic interface is implemented by arraylist ,even though they are same containers
+        // and contained types do have a relationship these two containers  dont have relationship ,thus cant A list of students cant be a reference type for an ArrayList of LPAStudents even though LPAStudent is a sub type of Students
+        int studentCount = 10;
+        List<Student> students = new ArrayList<>();
+        for (int i = 0; i < studentCount; i++) {
+            students.add(new Student());
+        }
+        students.add(new LPAStudent());
+//        printList(students);
+        printMoreLists(students);
+
+        List<LPAStudent> lpaStudents = new ArrayList<>();
+        for (int i = 0; i < studentCount; i++) {
+            lpaStudents.add(new LPAStudent());
+        }
+//        printList(lpaStudents);
+        printMoreLists(lpaStudents);
+
+        testList(new ArrayList<String>(List.of("Able", "Barry", "Charlie")));
+        testList(new ArrayList<Integer>(List.of(1, 2, 3)));
+
+    }
+
+    public static void printMoreLists(List<? extends Student> students) {
+
+        for (var student : students) {
+            System.out.println(student);
+        }
+        System.out.println();
+    }
+
+    public static void testList(List<?> list) {
+
+        for (var element : list) {
+            if (element instanceof String s) {
+                System.out.println("String: " + s.toUpperCase());
+            } else if (element instanceof Integer i) {
+                System.out.println("Integer: " + i.floatValue());
+            }
         }
     }
+
+
+    public static   void printList(List<Student> Students)
+    {
+
+    }
+}
