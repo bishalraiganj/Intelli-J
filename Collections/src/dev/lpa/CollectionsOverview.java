610eb64d15b6849189e66d1833c8b5514a7acdc7
bishalraiganj <bishaladhikaryreal@gmail.com>
2024-07-09 17:43:40 +0530

    collections L197

---
+++ Collections/src/dev/lpa/CollectionsOverview.java
@@ -0,0 +1,38 @@
+package dev.lpa;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CollectionsOverview {
+
+    public static void main(String... main)
+    {
+
+        List<String> list = new ArrayList<>();
+
+        String[] names = {"Anna","Bob","Carol","David","Edna"};
+        list.addAll(Arrays.asList(names));
+
+        System.out.println(list);
+
+        list.add("Fred");
+
+        list.addAll(Arrays.asList("George","Gary","Greece"));
+
+        System.out.println(list);
+
+        System.out.println("Is Gary in the list ?"+list.contains("Gary"));
+
+        list.removeIf((s)->s.charAt(0) == 'G');
+
+        System.out.println(list);
+
+        System.out.println("Gary is in the list ?"+ list.contains("Gary"));
+
+
+
+
+    }
+
+}
