c9ae3ebe31b89048ae6ce8ea253406ec000c8f9e
bishalraiganj <bishaladhikaryreal@gmail.com>
2024-07-16 22:07:21 +0530

    udemy 198 exercise

---
+++ Collections198Udemy/src/dev/lpa/Main.java
@@ -0,0 +1,23 @@
+package dev.lpa;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String... args)
+    {
+
+        List<Card> cards = Card.getStandardDeck();
+        Card.printDeck(cards);
+
+
+
+
+    }
+
+
+
+
+
+
+
+}
