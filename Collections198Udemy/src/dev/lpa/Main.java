4ca9cc0afecf23a2efbb2c954b68b43ee5657ed8
bishalraiganj <bishaladhikaryreal@gmail.com>
2024-07-21 12:41:59 +0530

    Methods on the helper class Collections

--- Collections198Udemy/src/dev/lpa/Main.java
+++ Collections198Udemy/src/dev/lpa/Main.java
@@ -1,9 +1,7 @@
 package dev.lpa;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import javax.swing.*;
+import java.util.*;
 
 public class Main {
     public static void main(String... args)
@@ -59,6 +57,97 @@ public class Main {
 
         Collections.reverse(standardDeck);
         Card.printDeck(standardDeck," Reversed StandardDeck",4);
+
+        var sortingAlgorithm = Comparator.comparing(Card::rank)
+                .thenComparing(Card::suit);
+
+        Collections.sort(standardDeck,sortingAlgorithm);
+        Card.printDeck(standardDeck," Standard Deck sorted by rank, suit",13);
+
+        Collections.reverse(standardDeck);
+        Card.printDeck(standardDeck,"Sorted by rank, suit reversed",13);
+
+        List<Card> kings = new ArrayList<>(standardDeck.subList(4,8));
+        Card.printDeck(kings," Kings in deck",1);
+
+        List<Card> tens = new ArrayList<>(standardDeck.subList(16,20));
+        Card.printDeck(tens," Tens in deck",1);
+
+        Collections.shuffle(standardDeck);
+        int subListIndex = Collections.indexOfSubList(standardDeck,tens);
+        System.out.println(" sublist index for tens = "+subListIndex);
+        System.out.println("Contains ="+standardDeck.containsAll(tens));
+
+        boolean disjoint =  Collections.disjoint(standardDeck,tens);
+        System.out.println(" disjoint ="+disjoint);
+
+        boolean disjoint2 = Collections.disjoint(kings,tens);
+        System.out.println(" disjoint2 ="+disjoint2);
+
+
+        Collections.sort(standardDeck,sortingAlgorithm);
+        Card tenOfHearts = Card.getNumericCard(Card.Suit.HEART,10);
+        int foundIndex = Collections.binarySearch(standardDeck,tenOfHearts,sortingAlgorithm);
+        System.out.println("foundIndex ="+foundIndex);
+        System.out.println("finding index using List.indexOf() ="+standardDeck.indexOf(tenOfHearts));
+        System.out.println(standardDeck.get(foundIndex));
+
+
+        Card tenOfClubs = Card.getNumericCard(Card.Suit.CLUB,10);
+        Collections.replaceAll(standardDeck,tenOfClubs,tenOfHearts);
+        Card.printDeck(standardDeck.subList(32,36),"ten of clubs being replaces with ten of hearts",1);
+
+        Collections.replaceAll(standardDeck,tenOfHearts,tenOfClubs);
+        Card.printDeck(standardDeck.subList(32,36)," ten of hearts being replaces with ten of clubs",1);
+
+        if(Collections.replaceAll(standardDeck,tenOfHearts,tenOfClubs))
+        {
+            System.out.println(" Tens of hearts replaced with tens of clubs");
+        }
+        else
+        {
+            System.out.println(" No tens of hearts found in the list");
+        }
+
+        System.out.println(" Ten of clubs cards ="+Collections.frequency(standardDeck,tenOfClubs));
+
+        System.out.println("Best Card ="+Collections.max(standardDeck,Comparator.comparing((Card s)->s.rank()).thenComparing((s)->s.suit())));/* Here in the lambda expression which is in the comparing() i have explicitly declared the type of formal parameter/ the type of the argument that is being
+        passed to the sam ,   sometimes lambdas type inference is bad specially in cases of using convenience methods and lambda chaining, because of bad type inference,  but
+        if the sam has only one method and its pre-defined on a class we can infer this using method reference, since method reference has better type inference and more concise , we can use method reference ,
+
+        here in this case the method Comparator.comparing() uses an implementation of the function functional interface whose sam has the method rank() invoked on the Card record instance passed as first argument to the sam
+        meaning we can replace that corresponding lambda with the following method reference Card::rank   , and the instance on which the method rank() is invoked is an unbounded retriever;
+        so the above method reference is an unbounded retriever instance method reference;   */
+
+        System.out.println(" Worst Card in the standard deck ="+Collections.min(standardDeck,Comparator.comparing(Card::rank).thenComparing(Card::suit)));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
 
 
 
