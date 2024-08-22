08efe6b62216faeca2b903f3c6480752cf8e2a34
bishalraiganj <bishaladhikaryreal@gmail.com>
2024-08-22 13:47:01 +0530

    main class added

--- Lecture204/src/dev/lpa/Contact.java
+++ Lecture204/src/dev/lpa/Contact.java
@@ -1,4 +1,66 @@
 package dev.lpa;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Contact {
+
+    private String name;
+    private Set<String> emails = new HashSet<>();
+    private Set<String> phones = new HashSet<>();
+
+
+    public Contact(String name)
+    {
+        this.name = name;
+
+    }
+    public Contact(String name,String email)
+    {
+        this.name =name;
+        this.emails.add(email);
+
+    }
+    public Contact(String name,long phone)
+    {
+        this.name = name;
+        this.phones.add(Long.toString(phone));
+    }
+    public Contact(String name,String email,long phone)
+    {
+        this.name = name;
+        this.emails.add(email);
+        this.phones.add(Long.toString(phone));
+    }
+
+
+    public String getName()
+    {
+        return name;
+
+    }
+
+    public String toString()
+    {
+        return "";
+    }
+
+    public Contact mergeContactData(Contact contact)
+    {
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 }
