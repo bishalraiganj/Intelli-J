87e691fe8cd4d1f1c9b41c1fee09f1c67cd132ab
bishalraiganj <bishaladhikaryreal@gmail.com>
2024-08-23 22:36:55 +0530

    now

--- SetsAndMaps/src/dev/lpa/dev/lpa/Contact.java
+++ SetsAndMaps/src/dev/lpa/dev/lpa/Contact.java
@@ -1,8 +1,50 @@
 package dev.lpa.dev.lpa;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Contact {
 
+   private String name;
+   private Set<String> emails = new HashSet<>();
+   private Set<String> phones = new HashSet<>();
 
+   public Contact(String name)
+   {
+       this(name,null);
+
+   }
+
+   public Contact(String name,String email)
+   {
+
+       this(name,email,0);
+
+   }
+
+   public Contact(String name,long phone)
+   {
+
+    this(name,null,phone);
+
+   }
+
+   public Contact(String name,String email,long phone)
+   {
+       this.name = name;
+       if(email!=null)
+       this.emails.add(email);
+       if(phone>0)
+       {
+           String p=String.valueOf(phone);
+           this.phones.add("(%s) %s-%s".formatted(p.substring(0,3),p.substring(3,6),p.substring(6)));
+
+       }
+
+
+
+
+   }
 
 
 }
