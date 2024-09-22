7ada1e445154a776b723a265eda0085f7a079f37
bishalraiganj <bishaladhikaryreal@gmail.com>
2024-09-22 13:42:33 +0530

    now

--- SetsAndMapsWithMapMethods/src/dev/lpa/MapViewsMain.java
+++ SetsAndMapsWithMapMethods/src/dev/lpa/MapViewsMain.java
@@ -1,9 +1,6 @@
 package dev.lpa;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 public class MapViewsMain {
 
@@ -33,6 +30,41 @@ public class MapViewsMain {
         contacts.forEach((k,v)->System.out.println(v));
 
 
+        System.out.println("\n\nPrinting copy of keys created by creating a" +
+                "new TreeSet object\n\n");
+        copyOfKeys.remove("Linus Van Pelt");
+        System.out.println(copyOfKeys);
+        contacts.forEach((k,v)->System.out.println(v));
+
+        keysView.retainAll(List.of("Linus Van Pelt","Charlie Brown","Robin Hood","Mickey Mouse"));
+        System.out.println(keysView);
+        contacts.forEach((k,v)->System.out.println(v));
+
+        keysView.clear();
+        System.out.println(contacts);
+
+        ContactData.getData("phone").forEach((c)->contacts.put(c.getName(),c));
+        ContactData.getData("email").forEach((c)->contacts.put((c).getName(),c));
+        System.out.println(keysView);
+
+        var values = contacts.values();
+        values.forEach((System.out::println));
+
+
+        values.retainAll(ContactData.getData("email"));
+        System.out.println(keysView);
+        contacts.forEach((k,v)-> System.out.println(v));
+
+        System.out.println("-".repeat(40));
+        List<Contact> list = new ArrayList<>(values);
+        list.sort(Comparator.comparing(Contact::getNameLastFirst));
+        list.forEach(c->System.out.println(c.getNameLastFirst()+": "+c));
+
+        System.out.println("-".repeat(40));
+        Contact first = list.get(0);
+        contacts.put(first.getNameLastFirst(),first);
+        values.forEach(System.out::println);
+        keysView.forEach(System.out::println);
 
 
     }
