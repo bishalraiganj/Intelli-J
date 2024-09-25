efc12db5b9d47d1c216c7d699a4b5b21835d53e5
bishalraiganj <bishaladhikaryreal@gmail.com>
2024-09-25 21:34:33 +0530

    half way done

--- ColossalCaveAdventureMyVersion/src/bishal/adhikary/Location.java
+++ ColossalCaveAdventureMyVersion/src/bishal/adhikary/Location.java
@@ -1,10 +1,33 @@
 package bishal.adhikary;
 
-public class Location {
+import java.util.Map;
+
+public  class Location {
 
 
     private String description;
-
+    private Map<String,String> nextPlace;
+    public Location(String description,Map<String,String> nextPlace)
+    {
+        this.description = description;
+        this.nextPlace = nextPlace;
+    }
+    public String getDescription()
+    {
+        return description;
+    }
+    public Map<String,String> getNextPlace()
+    {
+        return nextPlace;
+    }
+    public void setDescription(String description)
+    {
+        this.description = description;
+    }
+    public void setNextPlace(Map<String,String> nextPlace)
+    {
+        this.nextPlace = nextPlace;
+    }
 
 
 
--- ColossalCaveAdventureMyVersion/src/bishal/adhikary/Main.java
+++ ColossalCaveAdventureMyVersion/src/bishal/adhikary/Main.java
@@ -1,9 +1,47 @@
 package bishal.adhikary;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args)
     {
+
+        Map<String,Location> locations = new HashMap<>();
+
+        Scanner s = new Scanner(
+                    """
+                road ,   at  the end of the road ,W: hill ,E:well house ,S:valley ,N:forest
+                hill ,on top of hill with a view in all directions ,N:forest ,E:road
+                well house,inside a well house for a small spring,W:road,N:lake,S:stream
+                valley,   in a forest valley beside a tumbling stream,N:road,W:hill,E:stream
+                forest,at the edge of a thick dark forest,S:road,E:lake
+                lake,by an alpine lake surrounded by wildflowers,W:forest,S:well house 
+                stream,near a stream with a rocky bed,W:valley, N:well house
+                """);
+
+
+
+         while(s.hasNext()) // it will look for tokens created with the default whitespace delimitter a space
+        {
+
+                String line = s.nextLine();
+                String[] tokens = line.split(",");
+
+                Arrays.asList(tokens).replaceAll(String::trim);
+                System.out.println(Arrays.toString(tokens));
+//                System.out.println(tokens[1]);
+
+            }
+
+/*
+        System.out.println(Arrays.asList(t).forEacjArrays.asList(t).indexOf(m));
+*/
+
+        }
 
 
 
@@ -14,4 +52,5 @@ public class Main {
     }
 
 
-}
+
+
