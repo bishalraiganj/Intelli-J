d4aecaac343141724d6072abc299b519a2f517d9
bishalraiganj <bishaladhikaryreal@gmail.com>
2024-10-07 16:45:15 +0530

    now

--- SortedMaps/src/dev/lpa/Main.java
+++ SortedMaps/src/dev/lpa/Main.java
@@ -11,47 +11,44 @@ public class Main {
 
 
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
 
-        Course jmc = new Course("jmc101","Java Master Class","Java");
-        Course python = new Course("pyt101","Python Master Class","Python");
-
+        Course jmc = new Course("jmc101", "Java Master Class", "Java");
+        Course python = new Course("pyt101", "Python Master Class", "Python");
 
 
-        addPurchase("Mary Martin",jmc,129.99);
-        addPurchase("Andy Martin",jmc,139.99);
-        addPurchase("Mary Martin",python,149.99);
-        addPurchase("Joe Jones",jmc,149.99);
-        addPurchase("Bill Brown",python,159.99);
+        addPurchase("Mary Martin", jmc, 129.99);
+        addPurchase("Andy Martin", jmc, 139.99);
+        addPurchase("Mary Martin", python, 149.99);
+        addPurchase("Joe Jones", jmc, 149.99);
+        addPurchase("Bill Brown", python, 159.99);
 
-        addPurchase("Chuck Cheese",python,119.99);
-        addPurchase("Davey Jones",jmc,139.99);
-        addPurchase("Eva East",python,139.99);
-        addPurchase("Fred Forker",jmc,139.99);
-        addPurchase("Greg Brady",python,129.99);
+        addPurchase("Chuck Cheese", python, 119.99);
+        addPurchase("Davey Jones", jmc, 139.99);
+        addPurchase("Eva East", python, 139.99);
+        addPurchase("Fred Forker", jmc, 139.99);
+        addPurchase("Greg Brady", python, 129.99);
 
-        purchases.forEach((k,v)->System.out.println(k + ":" + v));
+        purchases.forEach((k, v) -> System.out.println(k + ":" + v));
         System.out.println("-".repeat(60));
-        students.forEach((k,v)->System.out.println(k + ":" + v));
+        students.forEach((k, v) -> System.out.println(k + ":" + v));
 
-        NavigableMap<LocalDate,List<Purchase>> datedPurchases = new TreeMap<>();
-        for(Purchase p : purchases.values())
-        {
-            datedPurchases.compute(p.purchaseDate(),(pdate,plist)->{
-            List<Purchase> list = (plist==null)?new ArrayList<>():plist;
-            list.add(p);
-            return list;
+        NavigableMap<LocalDate, List<Purchase>> datedPurchases = new TreeMap<>();
+        for (Purchase p : purchases.values()) {
+            datedPurchases.compute(p.purchaseDate(), (pdate, plist) -> {
+                List<Purchase> list = (plist == null) ? new ArrayList<>() : plist;
+                list.add(p);
+                return list;
             });
         }
-        datedPurchases.forEach((k,v)->System.out.println(k + ":" + v));
+        datedPurchases.forEach((k, v) -> System.out.println(k + ":" + v));
 
         int currentYear = LocalDate.now().getYear();
 
-        LocalDate firstDay = LocalDate.ofYearDay(currentYear,1);
+        LocalDate firstDay = LocalDate.ofYearDay(currentYear, 1);
         LocalDate week1 = firstDay.plusDays(7);
-        Map<LocalDate,List<Purchase>> week1Purchases = datedPurchases.headMap(week1);
-        Map<LocalDate,List<Purchase>> week2Purchases = datedPurchases.tailMap(week1);
+        Map<LocalDate, List<Purchase>> week1Purchases = datedPurchases.headMap(week1);
+        Map<LocalDate, List<Purchase>> week2Purchases = datedPurchases.tailMap(week1);
 
 //        System.out.println("-".repeat(60));
 //        week1Purchases.forEach((k,v)->System.out.println(k + ":" + v));
@@ -60,18 +57,39 @@ public class Main {
 //        System.out.println("-".repeat(70));
 
 
-        displayStats(1,week1Purchases);
-        displayStats(2,week2Purchases);
+        displayStats(1, week1Purchases);
+        displayStats(2, week2Purchases);
 
         System.out.println("-".repeat(50));
         LocalDate lastDate = datedPurchases.lastKey();
         var previousEntry = datedPurchases.lastEntry();
 
-        List<Purchase> lastDaysData = previousEntry.getValue();
-        System.out.println(lastDate + " purchases : " + lastDaysData.size());
+        while (previousEntry != null) {
+            List<Purchase> lastDaysData = previousEntry.getValue();
+            System.out.println(lastDate + " purchases : " + lastDaysData.size());
+
+            LocalDate prevDate = datedPurchases.lowerKey(lastDate);
+            previousEntry = datedPurchases.lowerEntry(previousEntry.getKey());
+            lastDate = prevDate;
+        }
+
+        System.out.println("-".repeat(60));
+        var reversed  = datedPurchases.descendingMap();
+
+        LocalDate firstDate = reversed.firstKey();
+        Map.Entry<LocalDate,List<Purchase>> nextEntry = reversed.firstEntry();
+
+        while (nextEntry != null)
+        {
+            List<Purchase> lastDaysData = nextEntry.getValue();
+            System.out.println(firstDate + " purchases : " + lastDaysData.size());
+
+            LocalDate nextDate = reversed.higherKey(firstDate);
+            nextEntry = reversed.higherEntry(firstDate);
+            firstDate = nextDate ;
 
 
-
+        }
 
 
 
