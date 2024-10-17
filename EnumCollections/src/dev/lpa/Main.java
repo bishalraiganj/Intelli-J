9951e6a4f83eb9479fac24f1e58facf51f27086f
bishalraiganj <bishaladhikaryreal@gmail.com>
2024-10-17 11:30:14 +0530

    now

--- EnumCollections/src/dev/lpa/Main.java
+++ EnumCollections/src/dev/lpa/Main.java
@@ -1,11 +1,21 @@
 package dev.lpa;
 
+
+import java.util.ArrayList;
+import java.util.EnumSet;
+import java.util.List;
+
 public class Main
 {
+
+    enum WeekDay{SUNDAY,MONDAY,TUESDAY,WEDNESDAY,THURSDAY,FRIDAY,SATURDAY}
 
     public static void main(String... args)
     {
 
+        List<WeekDay> annsWorkDays =  new ArrayList<>(List.of(WeekDay.MONDAY,WeekDay.TUESDAY,WeekDay.THURSDAY,WeekDay.FRIDAY));
 
+        var annsDaysSet = EnumSet.copyOf(annsWorkDays);
+        System.out.println(annsDaysSet.getClass().getSimpleName());
     }
 }
