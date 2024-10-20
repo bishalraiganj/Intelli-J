da2ed2b6e4aac3c9ca7d5c0aa7f27e6852d8b477
bishalraiganj <bishaladhikaryreal@gmail.com>
2024-10-20 12:08:11 +0530

    Created inventory in the Main method of the Main class

--- UdemyExercise222MyVersion/src/Adhikary/X/Main.java
+++ UdemyExercise222MyVersion/src/Adhikary/X/Main.java
@@ -1,11 +1,21 @@
 package Adhikary.X;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
 
     public static void main(String... args)
     {
+        Product adidasBoot= new Product("A_FG_01","Adidas FG Predator","Adidas","Shpoes");
+        Product niviaFootball = new Product("N_D_01","Nivia Dominator Football","Nivia","Football");
+        InventoryItem shoes = new InventoryItem(adidasBoot,23,0,10,5,3999.0);
+        InventoryItem footballs = new InventoryItem(niviaFootball,45,10,10,5,899.0);
+        Map<Product,InventoryItem> itemsInInventory = new HashMap<>();
+        itemsInInventory.put(adidasBoot,shoes);
+        itemsInInventory.put(niviaFootball,footballs);
 
-        InventoryItem shoes = new InventoryItem(new Product("")
+
 
 
     }
