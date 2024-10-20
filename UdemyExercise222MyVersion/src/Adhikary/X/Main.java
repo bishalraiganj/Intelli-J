702927f67abed4d9512d7be68505870e4e2c9f08
bishalraiganj <bishaladhikaryreal@gmail.com>
2024-10-20 11:08:55 +0530

    manageStoreCarts done ready for testing

--- UdemyExercise222MyVersion/src/Adhikary/X/Main.java
+++ UdemyExercise222MyVersion/src/Adhikary/X/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String... args)
     {
 
+        InventoryItem shoes = new InventoryItem(new Product("")
 
 
     }
--- UdemyExercise222MyVersion/src/Adhikary/X/Store.java
+++ UdemyExercise222MyVersion/src/Adhikary/X/Store.java
@@ -13,45 +13,38 @@ class IdComparator implements Comparator<Cart> {
 }
 
 public class Store {
-//    private IdComparator idComparator = new IdComparator();
-    private HashMap<Product,InventoryItem> inventory = new HashMap<>();
+    //    private IdComparator idComparator = new IdComparator();
+    private HashMap<Product, InventoryItem> inventory = new HashMap<>();
 //    private Set<Cart> carts = new TreeSet<>(Comparator.comparing( ( o1)->o1.getId()));  Here , We have a lambda expression inside the comparing method which has the function functional interface as its parameter in its method signature
 
     private Set<Cart> carts = new TreeSet<>(Comparator.comparing(Cart::getId));
 
-    private HashMap<Product,InventoryItem> aisleInventory  = new HashMap<>();
+    private HashMap<Product, InventoryItem> aisleInventory = new HashMap<>();
 
-    public Store(HashMap<Product,InventoryItem> inventory,HashMap<Product,InventoryItem> aisleInventory,Set<Cart> carts)
-    {
+    public Store(HashMap<Product, InventoryItem> inventory, HashMap<Product, InventoryItem> aisleInventory, Set<Cart> carts) {
         this.inventory = inventory;
         this.aisleInventory = aisleInventory;
         this.carts = carts;
     }
 
-    public void  manageStoreCarts(Cart c)
-    {
+    public void manageStoreCarts(Cart c) {
         Scanner s = new Scanner(System.in);
-        if(carts.contains(c))
-        {
-            System.out.println("Cart Already Exists! \n Manage Cart : "+ c.getId()+"\n Press 1.Add Cart \n 2.Remove From Cart");
+        if (carts.contains(c)) {
+            System.out.println("Cart Already Exists! \n Manage Cart : " + c.getId() + "\n Press 1.Add Cart \n 2.Remove From Cart");
 
             int input = s.nextInt();
-            if(input == 1)
-            {
+            if (input == 1) {
                 System.out.println("Enter the product name");
                 String productName = s.nextLine();
-                for(InventoryItem item: inventory.values() )
-                {
-                    if(item.getProduct().getName().equalsIgnoreCase(productName))
-                    {
+                for (InventoryItem item : inventory.values()) {
+                    if (item.getProduct().getName().equalsIgnoreCase(productName)) {
                         System.out.println("Product is Available in the Store \n " +
-                                "Current Stocks :"+(item.getQtyTotal()-item.getQtyReserved()));
-                        int qty =s.nextInt();
-                        if(item.getQtyTotal()-item.getQtyReserved()>=qty)
-                        {
+                                "Current Stocks :" + (item.getQtyTotal() - item.getQtyReserved()));
+                        int qty = s.nextInt();
+                        if (item.getQtyTotal() - item.getQtyReserved() >= qty) {
 
                             System.out.println("In Stock, Adding to Your Cart");
-                            c.addItem(item,qty);
+                            c.addItem(item, qty);
 
 
                         }
@@ -59,35 +52,63 @@ public class Store {
                 }
                 System.out.println(" Product not found");
 
-            }
-            else if(input ==2)
-            {
+            } else if (input == 2) {
                 System.out.println("Enter the product name ");
                 String productName = s.nextLine();
-                for(InventoryItem item: inventory.values())
-                {
-                    if(item.getProduct().getName().equalsIgnoreCase(productName))
-                    {
+                for (InventoryItem item : inventory.values()) {
+                    if (item.getProduct().getName().equalsIgnoreCase(productName)) {
                         System.out.println("Product is in the Store\n Enter the quantity to remove from the cart");
                         int qty = s.nextInt();
 
-                        c.removeItem(item,qty);
+                        c.removeItem(item, qty);
                     }
                 }
 
-            }
-            else
-            {
+            } else {
                 System.out.println("Select from the correct options");
             }
 
 
-        }
-        else
+        } else
             System.out.println("Cart not found ! Want to add the cart and Manage Y/N ?");
-            carts.add()
+        if (s.nextLine().equalsIgnoreCase("y")) {
+
+            carts.add(c);
+            System.out.println(" \n Manage Cart : " + c.getId() + "\n Press 1.Add Cart \n 2.Remove From Cart");
+
+            int input = s.nextInt();
+            if (input == 1) {
+                System.out.println("Enter the product name");
+                String productName = s.nextLine();
+                for (InventoryItem item : inventory.values()) {
+                    if (item.getProduct().getName().equalsIgnoreCase(productName)) {
+                        System.out.println("Product is Available in the Store \n " +
+                                "Current Stocks :" + (item.getQtyTotal() - item.getQtyReserved()));
+                        int qty = s.nextInt();
+                        if (item.getQtyTotal() - item.getQtyReserved() >= qty) {
+
+                            System.out.println("In Stock, Adding to Your Cart");
+                            c.addItem(item, qty);
 
 
+                        }
+                    }
+                }
+                System.out.println(" Product not found");
+
+            } else if (input == 2) {
+                System.out.println("Enter the product name ");
+                String productName = s.nextLine();
+                for (InventoryItem item : inventory.values()) {
+                    if (item.getProduct().getName().equalsIgnoreCase(productName)) {
+                        System.out.println("Product is in the Store\n Enter the quantity to remove from the cart");
+                        int qty = s.nextInt();
+
+                        c.removeItem(item, qty);
+                    }
+                }
+            }
+        }
     }
 
 
