d373707bf36098aacd9bed2ed39991242f983200
bishalraiganj <bishaladhikaryreal@gmail.com>
2024-10-31 15:03:15 +0530

    now

--- GoodString/src/Adhikary/X/Main.java
+++ GoodString/src/Adhikary/X/Main.java
@@ -10,7 +10,8 @@ public class Main {
     {
 
 
-        askInput();
+        System.out.println(askInput());
+
 
 
 
@@ -22,8 +23,8 @@ public class Main {
     private static Map<String,String> askInput()
     {
         Scanner s = new Scanner(System.in);
-        System.out.println("Enter the Good String \n rules : \n 1<=len(good string)<=100 \n 1<=len(name)<=10^4 \n Good String will consist of lower ,upper case alphabets, digits and symbols \n Name will consist of only space, lower, upper, case alphabets \n Characters are case sensitive \n "+
-                "The ASCII values for all the characters in the good string and name will be between 32 to 126 (both inclusive ) \n");
+        System.out.println(" \n rules for good String and name : \n 1<=len(good string)<=100 \n 1<=len(name)<=10^4 \n Good String will consist of lower ,upper case alphabets, digits and symbols \n Name will consist of only space, lower, upper, case alphabets \n Characters are case sensitive \n "+
+                "The ASCII values for all the characters in the good string and name will be between 32 to 126 (both inclusive ) \n \nEnter Your Good String \n");
 
         String goodString =  s.nextLine();
 //        if(goodString.length()<=100&&goodString.length()>=1&&goodString.chars().anyMatch((ch)->{ return "abcdefghijklmnopqrstuvwxyz".indexOf(ch)>=0
@@ -33,9 +34,67 @@ public class Main {
 
         System.out.println("Enter Your name");
         String name = s.nextLine();
-        Map<String,String> map = new TreeMap<>();
-        map.put(goodString,name);
-        return map;
+
+        if(name.length()<=10*10*10*10&&name.length()>=1)
+        {
+            boolean symbolRange1= false;
+            boolean symbolRange2= false;
+            boolean symbolRange3= false;
+            boolean symbolRange4= false;
+            boolean anySymbolRange= false;
+            for(int i=33;i<=47;i++)
+            {
+                if(name.contains(String.valueOf((char) i)))
+                {
+                    symbolRange1= true;
+                    anySymbolRange=true;
+                    break;
+                }
+            }
+            for(int i=58;i<=64;i++)
+            {
+                if(name.contains(String.valueOf((char) i))&&!anySymbolRange)
+                {
+                    symbolRange2=true;
+                    anySymbolRange=true;
+                    break;
+                }
+            }
+            for(int i=91;i<=96;i++)
+            {
+                if(name.contains(String.valueOf((char) i))&&!anySymbolRange)
+                {
+                    symbolRange3=true;
+                    anySymbolRange=true;
+                    break;
+                }
+            }
+            for(int i=123;i<=126;i++)
+            {
+                if(name.contains(String.valueOf((char) i))&&!anySymbolRange)
+                {
+                    symbolRange4=true;
+                    anySymbolRange=true;
+                    break;
+                }
+            }
+
+            if(!anySymbolRange)
+            {
+                Map<String,String> map = new TreeMap<>();
+                map.put(goodString,name);
+                System.out.println("Constraints followed Returning the map ");
+                return map;
+
+            }
+
+        }
+
+
+        System.out.println("Constraints not followed");
+        return null;
+
+
     }
 
     private static void findDistance(Map<String,String> map)
