aa92d782cf32fbbf27163addb2ab9ea6ae9d074c
bishalraiganj <bishaladhikaryreal@gmail.com>
2024-11-03 23:43:00 +0530

    now

---
+++ ZeroCount/src/ZeroCount.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class ZeroCount {
+    public static void main(String[] args)
+    {
+
+        Scanner s = new Scanner(System.in);
+        System.out.println("Specify the length of the Binary String");
+        int length = s.nextInt();
+        s.nextLine();
+        System.out.println("Specify the number of 1's in the Binary String");
+        int number = s.nextInt();
+        s.nextLine();
+
+        if(number<=length&&number>=0&&length<=Math.pow(10,6)&&length>=1)
+        {
+         int ic =1;
+         while(ic<=)
+
+
+
+
+        }
+        else {
+
+            System.out.println("Invalid Value");
+        }
+    }
+
+}
