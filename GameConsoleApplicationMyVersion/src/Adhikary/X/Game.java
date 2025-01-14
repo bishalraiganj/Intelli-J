69b1d38cd29fb83e5729f2ba1ab75fd3a611c3b3
bishalraiganj <bishaladhikaryreal@gmail.com>
2025-01-14 20:37:39 +0530

    adding the methods on the game class

--- GameConsoleApplicationMyVersion/src/Adhikary/X/Game.java
+++ GameConsoleApplicationMyVersion/src/Adhikary/X/Game.java
@@ -3,12 +3,33 @@ package Adhikary.X;
 import java.util.List;
 import java.util.Map;
 
-public class Game <T extends Player>    {
+public abstract class Game <T extends Player>    {
     private final String gameName ;
 
     private final List<T> players ;
 
-    private final Map<Character,Game>
+    private final Map<Character,GameAction>  standardActions ;
+
+    public Game(String gameName,List<T> players,Map<Character,GameAction> standardActions)
+    {
+        this.gameName = gameName;
+        this.players = players;
+        this.standardActions=standardActions;
+    }
+    public  abstract T createNewPlayer(String name);
+
+    public abstract Map<Character,GameAction> getGameActions(int playerIndex);
+
+    public int addPlayer(String name)
+    {
+        players.add()
+    }
+
+    public boolean executeGameAction(Character key)
+    {
+
+    }
+
 
 
 
