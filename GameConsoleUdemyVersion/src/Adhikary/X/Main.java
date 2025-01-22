91778951664b2ef8a02bb879057a42093df2994b
bishalraiganj <bishaladhikaryreal@gmail.com>
2025-01-22 08:36:09 +0530

    Pirate class complete

--- GameConsoleUdemyVersion/src/Adhikary/X/Main.java
+++ GameConsoleUdemyVersion/src/Adhikary/X/Main.java
@@ -1,13 +1,32 @@
 package Adhikary.X;
 
+import Adhikary.X.Pirate.Pirate;
+import Adhikary.X.Pirate.Weapon;
 import Adhikary.X.game.GameConsole;
 import Adhikary.X.game.Shooter;
 import Adhikary.X.game.ShooterGame;
 
+import java.util.List;
+
 public class Main {
     public static void main(String... args)
     {
-        GameConsole<ShooterGame> console = new GameConsole<>(new )
+//        GameConsole<ShooterGame> console = new GameConsole<>(new ShooterGame("" +
+//                "The Shootout Game") );
+//        int playerIndex = console.addPlayer();
+//        console.playGame(playerIndex);
+
+        Weapon weapon = Weapon.getWeaponByChar('P');
+        System.out.println("Weapon = "+ weapon + ", hitPoints=" + weapon.getHitPoints()+
+                ", minLevel = " + weapon.getMinLevel());
+
+        List<Weapon> list = Weapon.getWeaponsByLevel(1);
+        list.forEach(System.out::println);
+
+        Pirate tim = new Pirate("Tim");
+        System.out.println(tim);
+
+
 
 
     }
