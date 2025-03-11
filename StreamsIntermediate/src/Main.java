03fcb5ed32c4b0e36cee73f050b520ad26dd5514
bishalraiganj <bishaladhikaryreal@gmail.com>
2025-03-11 13:06:51 +0530

    Testing stream methods

--- StreamsIntermediate/src/Main.java
+++ StreamsIntermediate/src/Main.java
@@ -1,4 +1,6 @@
+import java.util.Random;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Main {
 
@@ -6,6 +8,24 @@ public class Main {
     {
 
         IntStream.iterate((int) 'A',(e)->e<=(int) 'z',(e)->e+1)
+                .filter(Character::isAlphabetic)
+//                .distinct()
+                .map(Character::toUpperCase)
+                .distinct()
+//                .dropWhile((e)->Character.toUpperCase(e)<='E')
+//                .takeWhile((e)->e< 'a')
+//                .skip(5)
+//              .filter((e)->Character.toUpperCase(e)> 'E')
+                .forEach((e)->System.out.printf("%c ",e));
+
+
+        System.out.println("\n"+"-".repeat(50));
+
+        Random random = new Random();
+        Stream.generate(()->random.nextInt((int) 'A',(int) 'Z'+1))
+                .limit(50)
+                .distinct()
+                .sorted()
                 .forEach((e)->System.out.printf("%c ",e));
 
 
