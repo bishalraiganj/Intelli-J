f587f77974e948f85f76b2559fd5e8757fe120c7
bishalraiganj <bishaladhikaryreal@gmail.com>
2025-03-18 17:22:36 +0530

    Seat custom constructor boolean value false

--- StreamTerminal/src/Adhikary/X/Seat.java
+++ StreamTerminal/src/Adhikary/X/Seat.java
@@ -7,7 +7,7 @@ public record Seat(char rowMarker, int seatNumber, boolean isReserved) {
     public Seat(char rowMarker,int seatNumber)
     {
 //        this(rowMarker,seatNumber,new Random().nextBoolean());
-        this(rowMarker,seatNumber,true);
+        this(rowMarker,seatNumber,false);
     }
 
 }
