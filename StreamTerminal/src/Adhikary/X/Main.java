76107b12803a7f51dd136050dc899f94f43a1c86
bishalraiganj <bishaladhikaryreal@gmail.com>
2025-03-18 17:21:40 +0530

    custom constructor boolean value true

--- StreamTerminal/src/Adhikary/X/Main.java
+++ StreamTerminal/src/Adhikary/X/Main.java
@@ -20,7 +20,27 @@ public class Main {
 
         Seat[] seats = new Seat[100];
         Arrays.setAll(seats,(i)->new Seat((char) ('A'+i/10),i%10+1));
-        Arrays.asList(seats).forEach(System.out::println);
+//        Arrays.asList(seats).forEach(System.out::println);
+
+        long reservationCount = Arrays
+                .stream(seats)
+                .filter(Seat::isReserved)
+                .count();
+        System.out.println("reservationCount = " + reservationCount);
+
+        boolean hasBookings = Arrays.stream(seats)
+                .anyMatch(Seat::isReserved);
+        System.out.println("hasBookings = " + hasBookings);
+
+        boolean fullyBooked = Arrays
+                .stream(seats)
+                .allMatch(Seat::isReserved);
+        System.out.println("fullyBooked = " + fullyBooked);
+
+        boolean eventWashedOut = Arrays
+                .stream(seats)
+                .noneMatch(Seat::isReserved);
+        System.out.println("eventWashedOut = " + eventWashedOut);
 
 
 
--- StreamTerminal/src/Adhikary/X/Seat.java
+++ StreamTerminal/src/Adhikary/X/Seat.java
@@ -6,7 +6,8 @@ public record Seat(char rowMarker, int seatNumber, boolean isReserved) {
 
     public Seat(char rowMarker,int seatNumber)
     {
-        this(rowMarker,seatNumber,new Random().nextBoolean());
+//        this(rowMarker,seatNumber,new Random().nextBoolean());
+        this(rowMarker,seatNumber,true);
     }
 
 }
