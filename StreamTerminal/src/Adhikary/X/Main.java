5f6c867350f23f9ace0a28b24083fd2390d91092
bishalraiganj <bishaladhikaryreal@gmail.com>
2025-03-18 16:33:49 +0530

    Terminal Operations

---
+++ StreamTerminal/src/Adhikary/X/Main.java
@@ -0,0 +1,16 @@
+package Adhikary.X;
+
+import java.util.stream.IntStream;
+
+public class Main {
+    public static void main(String... args)
+    {
+        var  result = IntStream.iterate(0,(i)->i<=1000,(i)->i+10)
+                .summaryStatistics();
+
+        System.out.println("Results = "+result);
+
+
+
+    }
+}
