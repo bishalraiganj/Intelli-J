b968989763906ee05740108142c84706c18b8b1f
bishalraiganj <bishaladhikaryreal@gmail.com>
2025-03-18 16:47:27 +0530

    Leap Years from 2000-2025

--- StreamTerminal/src/Adhikary/X/Main.java
+++ StreamTerminal/src/Adhikary/X/Main.java
@@ -5,10 +5,16 @@ import java.util.stream.IntStream;
 public class Main {
     public static void main(String... args)
     {
-        var  result = IntStream.iterate(0,(i)->i<=1000,(i)->i+10)
+        var  result = IntStream.iterate(0,(i)->i<=1000,(i)->i+3)
                 .summaryStatistics();
 
         System.out.println("Results = "+result);
+
+        var leapYearData = IntStream.iterate(2000,(i)->i<=2025,(i)->i+1)
+                .filter((i)->i%4==0)
+                .peek(System.out::println)
+                .summaryStatistics();
+
 
 
 
