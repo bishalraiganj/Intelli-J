8a09f2585a4554452b86c4fdeb173cca870a6900
bishalraiganj <bishaladhikaryreal@gmail.com>
2025-03-28 16:09:05 +0530

    Student Constructor Created

---
+++ StudentEngagementSystem/src/Student.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+public class Student {
+
+    private static long lastStudentId = 1;
+    private final static Random random = new Random();
+
+    private final long studentId;
+    private final String countryCode;
+    private final int yearEnrolled;
+    private final int ageEnrolled;
+    private final String gender;
+    private final Boolean programmingExperience;
+
+    private final Map<String,CourseEngagement> engagementMap = new HashMap<>();
+
+    public Student(String countryCode,int yearEnrolled,int ageEnrolled,String gender,boolean programmingExperience,Course... courses)
+    {
+        studentId = lastStudentId++;
+        this.countryCode = countryCode;
+        this.yearEnrolled  = yearEnrolled;
+        this.ageEnrolled = ageEnrolled;
+        this.gender   = gender;
+        this.programmingExperience = programmingExperience;
+
+    }
+
+
+}
