ba6788cf1ee0a703a14914cb770c298201f49fd7
bishalraiganj <bishaladhikaryreal@gmail.com>
2025-03-28 20:42:50 +0530

    tests

--- StudentEngagementSystem/src/Main.java
+++ StudentEngagementSystem/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.stream.Stream;
+
 public class Main {
 
     public static void main(String... args)
@@ -5,14 +7,17 @@ public class Main {
 
         Course pymc = new Course("PYMC","Python Masterclass");
         Course jmc = new Course("JMC","Java Masterclass");
-        Student bishal = new Student("IN",2018,22,"M",true,jmc,pymc);
+//        Student bishal = new Student("IN ",2018,22,"M",true,jmc,pymc);
+//
+//        System.out.println(bishal);
 
-        System.out.println(bishal);
+//        bishal.watchLecture("JMC",10,5,2019);
+//        bishal.watchLecture("PYMC",7,7,2020);
+//        System.out.println(bishal);
 
-        bishal.watchLecture("JMC",10,5,2019);
-        bishal.watchLecture("PYMC",7,7,2020);
-        System.out.println(bishal);
-
+        Stream.generate(()->Student.getRandomStudent(jmc,pymc))
+                .limit(10)
+                .forEach(System.out::println);
 
     }
 
