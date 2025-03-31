f32b375133aaf5f43920cda59f29c694a441265e
bishalraiganj <bishaladhikaryreal@gmail.com>
2025-03-31 10:27:18 +0530

    sorted students by longest enrolled and still active

---
+++ StudentEngagementSystem/src/MainChallenge.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+public class MainChallenge {
+
+    public static void main(String... args)
+    {
+        Course pymc = new Course("PYMC","Python Masterclass",50);
+        Course jmc = new Course("JMC","Java Masterclass",100);
+        Course jgms= new Course("JGMS","Creating Games in Java");
+
+        List<Student> studentsList = Stream.generate(()->Student.getRandomStudent(jmc,pymc))
+                .limit(5000)
+                .collect(()->new ArrayList<>(),(List<Student> e1,Student e2)->e1.add(e2),(List<Student> e3,List<Student> e4)->e3.addAll(e4));
+
+
+        double  average = studentsList.stream()
+                .map((e)->(double)e.getPercentComplete("JMC"))
+                .reduce((double) 0,Double::sum);
+        System.out.println(average/5000);
+
+        List<Student> longestStudents = studentsList.stream()
+                .filter((e)->(e.getMonthsSinceActive()<12))
+                .sorted(Comparator.comparing(Student::getYearsSinceEnrolled).reversed())
+                .limit(10)
+                .map((e)->{
+                    e.addCourse(jgms);
+                    return e;
+                })
+                .collect(()->new ArrayList<>(),(e1,e2)->e1.add(e2),(e3,e4)->e3.addAll(e4));
+
+        longestStudents.forEach(System.out::println);
+        System.out.println("\n"+"-".repeat(50));
+
+
+
+
+
+
+
+    }
+
+}
--- StudentEngagementSystem/src/MainCollect.java
+++ StudentEngagementSystem/src/MainCollect.java
@@ -62,5 +62,12 @@ public class MainCollect {
 
 
 
+
+
+
+
+
+
+
     }
 }
