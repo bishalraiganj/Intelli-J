c053eb2639d2d09a91823b6a662b0a35af145dbb
bishalraiganj <bishaladhikaryreal@gmail.com>
2025-04-02 11:05:54 +0530

    used the flatMap() method to flatten streams of collections to a single stream of the collections elements:

--- StudentEngagementSystem/src/MainMapping.java
+++ StudentEngagementSystem/src/MainMapping.java
@@ -69,6 +69,40 @@ public class MainMapping {
 
 
 
+            long studentBodyCount =0;
+            for (List<Student> list : experienced.values())
+            {
+                studentBodyCount += list.size();
+            }
+            System.out.println("studentBodyCount  = " + studentBodyCount);
+
+            studentBodyCount = experienced.values().stream()
+                    .mapToInt((l)->l.size())
+                    .sum();
+            System.out.println("studentBodyCount = "+ studentBodyCount);
+
+            studentBodyCount = experienced.values().stream()
+                    .map((l)->l.stream().filter((e)->e.getMonthsSinceActive()<=3).count())
+                    .mapToLong((l)->l)
+                    .sum();
+            System.out.println("StudentBodyCount = " + studentBodyCount);
+
+            System.out.println("-".repeat(50)+"\n Used the flatmap() method here \n");
+
+            long count = experienced.values().stream()
+                    .flatMap((l)->l.stream())
+                    .filter((e)->e.getMonthsSinceActive()<=3)
+                    .count();
+            System.out.println("Active Students = " + count);
+
+            count = multiLevel.values().stream()
+                    .flatMap((m)->m.values().stream().flatMap((l)->l.stream()))
+                    .filter((e)->e.getMonthsSinceActive()<=3)
+                            .count();
+            System.out.println(" Active Students (Used Multi-Level Map) = " +count);
+
+
+
 
         }
 
