3e611bfe287766fe03e817f570087319b0ad9945
bishalraiganj <bishaladhikaryreal@gmail.com>
2025-04-11 02:49:52 +0530

    Project : Scheduling dates of different Time Zones

--- DateTimeLocalizationProject/src/Adhikary/X/Main.java
+++ DateTimeLocalizationProject/src/Adhikary/X/Main.java
@@ -1,10 +1,16 @@
 package Adhikary.X;
 
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
+import java.time.*;
 import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
 import java.time.zone.ZoneRules;
+import java.util.List;
 import java.util.Locale;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Main {
 
@@ -34,13 +40,62 @@ public class Main {
     {
 
         Employee  jane = new Employee("Jane",Locale.US,"America/New_York");
-        Employee joe = new Employee("Joe","en_AU","Australia/Sydney");
+        Employee joe = new Employee("Joe","en_AU","Australia/Eucla");
 
         ZoneRules joesRules = joe.zone.getRules();
         ZoneRules janesRules = jane.zone.getRules();
 
         System.out.println(jane + " " + janesRules);
         System.out.println(joe + " " + joesRules);
+
+        ZonedDateTime janeNow = ZonedDateTime.now(jane.zone);
+        ZonedDateTime joeNow = ZonedDateTime.of(janeNow.toLocalDateTime(),joe.zone);
+        long hoursBetween = Duration.between(joeNow,janeNow).toHours();
+        long minutesBetween = Duration.between(joeNow,janeNow).toMinutes();
+        System.out.println("Joe is " + Math.abs(hoursBetween) + " hours " + Math.abs(minutesBetween) + " minutes " + ((hoursBetween < 0 )? "behind" : "ahead"));
+
+        System.out.println("Joe in Daylight savings? " +
+                joesRules.isDaylightSavings(joeNow.toInstant())+" "+
+                        joesRules.getDaylightSavings(joeNow.toInstant())+": "+
+                        joeNow.format(DateTimeFormatter.ofPattern("zzzz z")));
+
+
+        System.out.println("Jane in Daylight savings? " +
+                janesRules.isDaylightSavings(janeNow.toInstant())+" "+
+                janesRules.getDaylightSavings(janeNow.toInstant())+": "+
+                janeNow.format(DateTimeFormatter.ofPattern("zzzz z")));
+        int days = 10;
+        Map<LocalDate,List<ZonedDateTime>> map = schedule(joe,jane,days);
+        DateTimeFormatter dtf = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.FULL,FormatStyle.SHORT);
+        for(LocalDate ldt : map.keySet())
+        {
+            System.out.println(ldt.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL)));
+            for(ZonedDateTime zdt : map.get(ldt))
+            {
+                System.out.println("\t" +
+                        jane.getDateInfo(zdt,dtf) + " <--> " + joe.getDateInfo(zdt.withZoneSameInstant(joe.zone),dtf));
+            }
+        }
+
+
+    }
+
+    private static Map<LocalDate, List<ZonedDateTime>> schedule(Employee first,Employee second,int days)
+    {
+
+        Predicate<ZonedDateTime> rules = (zdt)->zdt.getDayOfWeek() != DayOfWeek.SATURDAY
+                && zdt.getDayOfWeek() != DayOfWeek.SUNDAY
+                && zdt.getHour() >= 7 && zdt.getHour() <= 21;
+
+        LocalDate startingDate = LocalDate.now().plusDays(2);
+        return startingDate.datesUntil(startingDate.plusDays(days + 1))
+                .map((e)->e.atStartOfDay(first.zone()))
+                .flatMap((e)-> IntStream.range(0,24).mapToObj((e1)->e.withHour(e1)))
+                .filter(rules)
+                .map((e)->e.withZoneSameInstant(second.zone))
+                .filter(rules)
+                .collect(Collectors.groupingBy((ZonedDateTime e)->e.toLocalDate(),()->new TreeMap<>(),Collectors.toList()));
+
 
 
     }
