33067318578247ee712af6beb860773b315292dc
bishalraiganj <bishaladhikaryreal@gmail.com>
2025-04-10 22:19:52 +0530

    BigDecimal with Locale

--- LocaleProject/src/Adhikary/X/Main.java
+++ LocaleProject/src/Adhikary/X/Main.java
@@ -1,10 +1,15 @@
 package Adhikary.X;
 
+import java.math.BigDecimal;
+import java.text.NumberFormat;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
+import java.util.Currency;
 import java.util.List;
 import java.util.Locale;
+import java.util.Scanner;
 
 public class Main {
 
@@ -30,6 +35,37 @@ public class Main {
                 System.out.println(locale.getDisplayName() + "= " + LocalDateTime.now().format(dtf.withLocale(locale)));
 
         }
+
+        System.out.println("-".repeat(50));
+
+        DateTimeFormatter wdayMonth = DateTimeFormatter.ofPattern("EEEE, MMMM d, yyyy");
+
+        LocalDate May5 = LocalDate.of(2020,5,5);
+
+        System.out.println("-".repeat(50));
+        for(Locale locale : List.of(Locale.CANADA,Locale.CANADA_FRENCH,Locale.FRANCE,Locale.GERMANY,Locale.TAIWAN,Locale.JAPAN,Locale.ITALY))
+        {
+            System.out.println(
+                    locale.getDisplayName() + " : " +
+                            locale.getDisplayName(locale) + "=\n\t" +
+                            May5.format(wdayMonth.withLocale(locale)));
+            System.out.printf(locale,"\t%1$tA, %1$tB %1$te, %1$tY %n",May5);
+            NumberFormat decimalInfo = NumberFormat.getNumberInstance(locale);
+            decimalInfo.setMaximumFractionDigits(6);
+            System.out.println(decimalInfo.format(123456789.123456));
+            NumberFormat currency = NumberFormat.getCurrencyInstance(locale);
+            Currency localCurrency = Currency.getInstance(locale);
+            System.out.println(currency.format(555.555) + " [" + localCurrency.getCurrencyCode() + "] " +
+                    localCurrency.getDisplayName(locale) + "/" +
+                    localCurrency.getDisplayName());
+        }
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the loan amount: ");
+        scanner.useLocale(Locale.ITALY);
+        BigDecimal myLoan = scanner.nextBigDecimal();
+        NumberFormat decimalInfo = NumberFormat.getNumberInstance(Locale.ITALY);
+        System.out.println("My Loan " + decimalInfo.format(myLoan));
 
     }
 }
