14cd7e7f7a0b761fe0beb0f65ec42d0cd970c215
bishalraiganj <bishaladhikaryreal@gmail.com>
2025-04-11 20:11:52 +0530

    Practice for the locale fr_FR

---
+++ ResourceBundleProject/src/Adhikary/X/Main.java
@@ -0,0 +1,24 @@
+package Adhikary.X;
+
+import java.util.ResourceBundle;
+
+public class Main {
+
+    public static void main(String... args)
+    {
+        ResourceBundle rb = ResourceBundle.getBundle("BasicText");
+        System.out.println(rb.getClass().getName());
+        System.out.println(rb.getBaseBundleName());
+        System.out.println(rb.keySet());
+
+        System.out.printf("%s %s!%n",rb.getString("hello"),rb.getString("world"));
+        
+
+
+
+
+
+    }
+
+
+}
