6f723925fe24c713880984149683d366b11a0617
bishalraiganj <bishaladhikaryreal@gmail.com>
2025-04-17 17:12:18 +0530

    complex regex pattern for htmltags

--- PatternMatching/src/Adhikary/X/Review.java
+++ PatternMatching/src/Adhikary/X/Review.java
@@ -37,10 +37,14 @@ public class Review {
 
 
 
+
         phoneMatcher.reset();
         phoneMatcher.results()
                 .forEach((mr)->System.out.println(mr.group()));
         System.out.println("-".repeat(50));
+
+
+
 
 
         phoneMatcher.reset();
@@ -49,11 +53,20 @@ public class Review {
                 .forEach((mr)->System.out.println(mr.group()));
         System.out.println("-".repeat(50));
 
+
+
+
+
+
         phoneMatcher.reset();
         phoneMatcher.usePattern(Pattern.compile("\\(*[0-9]{3}\\)* *[0-9]{3}-[0-9]{4}"));
         phoneMatcher.results()
                 .forEach((mr)->System.out.println(mr.group()));
         System.out.println("-".repeat(50));
+
+
+
+
 
         phoneMatcher.reset();
         phoneMatcher.usePattern(Pattern.compile("\\(*[0-9]{3}[)\\s]*[0-9]{3}-[0-9]{4}"));
@@ -61,6 +74,57 @@ public class Review {
         {
             System.out.println(phoneMatcher.group());
         }
+        System.out.println("-".repeat(50));
+
+
+
+
+        phoneMatcher.reset();
+        phoneMatcher.usePattern(Pattern.compile("\\(*[0-9]{3}[)\\s-]*[0-9]{3}[\\s-]*[0-9]{4}"));
+        phoneMatcher.results()
+                .forEach((mr)->System.out.println(mr.group()));
+        System.out.println("-".repeat(50));
+
+
+
+        phoneMatcher.reset();
+        phoneMatcher.usePattern(Pattern.compile("\\(*[0-9]{3}[-\\s)]*\\d{3}[\\s-]*\\p{Digit}{4}"));
+        phoneMatcher.results()
+                .forEach((mr)->System.out.println(mr.group()));
+        System.out.println("-".repeat(50));
+
+
+        String htmlSnippets = """
+                 <H1>My Heading</H1>
+                 <h2>Sub-heading</h2>
+                 <p>This is a paragraph about something.</p>
+                 <p style="abc">This is another paragraph about something else.</p>
+                 <h3 id="third">Summary</h3>
+                 <br/>
+                 <p>Testing</p>
+                 """;
+
+
+
+
+        Pattern htmlPattern = Pattern.compile("<(\\w+)[^>]*>([^\\v</>]*)(</\\1>)*");
+        Matcher m = htmlPattern.matcher(htmlSnippets);
+        m.results()
+                .forEach((mr)->System.out.println("Full tag:" + mr.group(0)
+                        + "\n\tType: " +mr.group(1)
+                        + "\n\tText: " +mr.group(2)));
+
+        System.out.println("-".repeat(50));
+
+
+        m.reset();
+        m.usePattern(Pattern.compile("<([a-zA-Z_0-9]+)[^>]*>([^\r\n</>]*)(/\\1>)*")); // here , I have replaced \\w with the character class [a-zA-Z_0-9] and \\v in second group with \r and \n
+        m.results()
+                .forEach((mr)->System.out.println("Full tag:" + mr.group(0)
+                        + "\n\tType: " + mr.group(1)
+                        + "\n\tText: " + mr.group(2)
+                ));
+
 
 
 
