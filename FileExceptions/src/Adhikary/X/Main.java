893d71a158c42d193c8e8ec82802c774c7fd5805
bishalraiganj <bishaladhikaryreal@gmail.com>
2025-04-20 15:18:30 +0530

    exception handling

---
+++ FileExceptions/src/Adhikary/X/Main.java
@@ -0,0 +1,93 @@
+package Adhikary.X;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Main {
+
+	public static void main(String... args)  {
+		String filename = "testing.csv";
+		Path path = Paths.get(filename);
+
+
+//		try {
+//			List<String> lines = Files.readAllLines(path);
+//		} catch (IOException e)
+//		{
+//			throw new RuntimeException(e);
+//		}
+//
+
+		testFile(filename);
+		testFile2(filename);
+		File file = new File(filename);
+		if(!file.exists())
+		{
+			System.out.println("I can't run unless this file exists");
+			System.out.println("Quitting Application, go figure it out");
+			return;
+		}
+		System.out.println("I'm good to go.");
+
+
+	}
+
+	private static void testFile(String filename)
+	{
+		Path path = Paths.get(filename);
+		FileReader reader = null;
+		try
+		{
+//			List<String> lines = Files.readAllLines(path);
+			reader = new FileReader(filename);
+		} catch(IOException e)
+		{
+			throw new RuntimeException(e);
+//			int i = 1/0;
+		}
+		finally {
+			if(reader != null)
+			{
+				try {
+					reader.close();
+				} catch(IOException e)
+				{
+					throw new RuntimeException(e);
+				}
+			}
+			System.out.println("Maybe I'd log something either way...");
+		}
+		System.out.println("File exists and able to use as a resource");
+	}
+
+
+
+	private static void testFile2(String filename)
+	{
+		try(FileReader reader = new FileReader(filename))
+		{
+		}catch (FileNotFoundException e)
+		{
+			throw new RuntimeException(e);
+		}
+		catch(IOException e)
+		{
+			throw new RuntimeException(e);
+		}
+		finally {
+			System.out.println("Maybe I'd log something either way...");
+		}
+			System.out.println("File exists and able to use as a resources");
+
+
+
+
+	}
+
+}
