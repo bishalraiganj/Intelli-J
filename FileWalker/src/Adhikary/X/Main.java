61cef3f21939a48dbf2046998dc80c6bb5ced6cc
bishalraiganj <bishaladhikaryreal@gmail.com>
2025-04-23 21:07:58 +0530

    testing ok

--- FileWalker/src/Adhikary/X/Main.java
+++ FileWalker/src/Adhikary/X/Main.java
@@ -1,35 +1,38 @@
 package Adhikary.X;
 
 import java.io.IOException;
-import java.nio.file.FileVisitResult;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.SimpleFileVisitor;
+import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.Objects;
 import java.util.stream.Stream;
 
 public class Main {
 
-	public static void main(String... args)
-	{
+	public static void main(String... args) {
 
 		System.out.println("-".repeat(50));
-		Path cwd =  Path.of(".");
+		Path cwd = Path.of(".");
 
-		try( Stream<Path> cwdHierarchy = Files.find(cwd,Integer.MAX_VALUE,(path, attr)->Files.isDirectory(path)))
-		{
+		try (Stream<Path> cwdHierarchy = Files.find(cwd, Integer.MAX_VALUE, (path, attr) -> Files.isDirectory(path))) {
 
 			cwdHierarchy.
 					forEach(System.out::println);
 
-		}
-		catch (IOException e)
-		{
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
 		System.out.println("-".repeat(50));
+
+		Path startingPath = Path.of(".");
+
+		FileVisitor<Path> statsVisitor = new Main.StatsVisitor();
+		try {
+			Files.walkFileTree(startingPath, statsVisitor);
+		} catch (IOException e)
+		{
+			throw new RuntimeException(e);
+		}
 
 
 
@@ -40,15 +43,16 @@ public class Main {
 	}
 
 
-	public static  class statsVisitor extends SimpleFileVisitor<Path>
+	public static  class StatsVisitor extends SimpleFileVisitor<Path>
 	{
 
+		private int level;
 		@Override
 		public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException
 		{
 				Objects.requireNonNull(file);
 				Objects.requireNonNull(attrs);
-				System.out.println(file.getFileName());
+				System.out.println("\t".repeat(level + 1) + file.getFileName());
 				return FileVisitResult.CONTINUE;
 		}
 
@@ -58,12 +62,26 @@ public class Main {
 		{
 			Objects.requireNonNull(dir);
 			Objects.requireNonNull(attrs);
-			System.out.println(dir.getFileName());
+			level++;
+			System.out.println("\t".repeat(level) + dir.getFileName());
+			return FileVisitResult.CONTINUE;
+		}
+
+		@Override
+		public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException
+		{
+			Objects.requireNonNull(dir);
+//			if(exc != null )
+//			{
+//				throw exc;
+//			}
+			level--;
 			return FileVisitResult.CONTINUE;
 
-
-
 		}
+
+
+
 	}
 
 
