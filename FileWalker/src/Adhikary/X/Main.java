7b6fa13967d6326fa49ac6edf157a9ab1298ce6e
bishalraiganj <bishaladhikaryreal@gmail.com>
2025-04-25 11:52:19 +0530

    fixed the bug , sub-folders total size calculation : whenever a new file visited sub folder size was not set in the folderSizes map

--- FileWalker/src/Adhikary/X/Main.java
+++ FileWalker/src/Adhikary/X/Main.java
@@ -1,5 +1,6 @@
 package Adhikary.X;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
@@ -10,34 +11,49 @@ public class Main {
 
 	public static void main(String... args) {
 
+//		System.out.println("-".repeat(50));
+//		Path cwd = Path.of(".");
+//
+//		try (Stream<Path> cwdHierarchy = Files.find(cwd, Integer.MAX_VALUE, (path, attr) -> Files.isDirectory(path))) {
+//
+//			cwdHierarchy.
+//					forEach(System.out::println);
+//
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//
+//		System.out.println("-".repeat(50));
+//
+//		Path startingPath = Path.of(".");
+//
+//		FileVisitor<Path> statsVisitor = new Main.StatsVisitor();
+//		try {
+//			Files.walkFileTree(startingPath, statsVisitor);
+//		} catch (IOException e)
+//		{
+//			throw new RuntimeException(e);
+//		}
+
+
+
+		// The above logic is for testing through Hard Code :-)
+
+
 		System.out.println("-".repeat(50));
-		Path cwd = Path.of(".");
 
-		try (Stream<Path> cwdHierarchy = Files.find(cwd, Integer.MAX_VALUE, (path, attr) -> Files.isDirectory(path))) {
-
-			cwdHierarchy.
-					forEach(System.out::println);
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-		System.out.println("-".repeat(50));
-
-		Path startingPath = Path.of(".");
-
-		FileVisitor<Path> statsVisitor = new Main.StatsVisitor();
-		try {
-			Files.walkFileTree(startingPath, statsVisitor);
-		} catch (IOException e)
-		{
-			throw new RuntimeException(e);
-		}
-
-		System.out.println("-".repeat(50));
 
 
 		Scanner sc = new Scanner(System.in);
+
+		System.out.printf("""
+		                                         Greetings ! from Bishal Adhikary :-)    
+		                                         
+		                               Enter The File Path below (must be an absolute path ;-)  
+		
+		
+		
+		""");
 
 		String line = sc.nextLine();
 
@@ -60,7 +76,7 @@ public class Main {
 	public static  class StatsVisitor extends SimpleFileVisitor<Path>
 	{
 
-		private int outStart =0;
+//		private int outStart =0;
 		private Path initialPath = null;
 
 		private final Map<Path, List<Long>> folderSizes = new LinkedHashMap<>();
@@ -82,9 +98,10 @@ public class Main {
 			folderSizes.merge(file.getParent(),l,(o,n)->
 			{
 				    List<Long> list = new ArrayList<>();
-					list.addAll(l);
+					list.add(0,0L);
+					list.add(1,0L);
 					list.set(0,folderSizes.get(file.getParent()).get(0)+attrs.size());
-//					list.set(1,folderSizes.get(file.getParent()).get(1)+attrs.size()); // wrong ,
+					list.set(1,folderSizes.get(file.getParent()).get(1)); //  , here , the bug were  when we visited a file  list of values , at index 1 it was always zero , now setting value at 1 fixed the problem
 					return list;
 			});
 				return FileVisitResult.CONTINUE;
@@ -109,7 +126,7 @@ public class Main {
 				if (relativeLevel == 1)
 				{
 					folderSizes.clear();
-					outStart = 0;
+//					outStart = 0;
 				}
 				List<Long> list = new ArrayList<>();
 				list.add(0,0L);
@@ -139,6 +156,42 @@ public class Main {
 			int relativeLevel =  dir.getNameCount()-initialCount;
 			if(relativeLevel==1)
 			{
+
+
+
+				//
+
+
+//				List<Long> list = new ArrayList<>();
+//				list.add(0,0L);
+//				list.add(1,0L);
+////				if(outStart ==0)
+////				{
+////					long firstOutSize = folderSizes.get(dir).get(0);
+////					folderSizes.merge(dir.getParent(),list,(o,n)->{
+////						List<Long> l = new ArrayList<>();
+////						l.add(0,o.get(0));
+////						l.add(1,o.get(1)+firstOutSize);
+////						return l;
+////
+////					});
+////					outStart++;
+////				}
+////				else {
+//				long folderSize = folderSizes.get(dir).get(0);
+//				long subFolderSize = folderSizes.get(dir).get(1);
+//
+//				folderSizes.merge(dir.getParent(), list, (o, n) ->
+//
+//				{
+//					List<Long> l = new ArrayList<>();
+//					l.add(0, o.get(0));
+//					l.add(1, o.get(1) + folderSize + subFolderSize);
+//					return l;
+//
+//				});
+				//
+
 				folderSizes.forEach((key,value)->{
 
 					int level = key.getNameCount() - initialCount-1;
@@ -153,33 +206,36 @@ public class Main {
 				List<Long> list = new ArrayList<>();
 				list.add(0,0L);
 				list.add(1,0L);
-				if(outStart ==0)
-				{
-					long firstOutSize = folderSizes.get(dir).get(0);
-					folderSizes.merge(dir.getParent(),list,(o,n)->{
-						List<Long> l = new ArrayList<>();
-						l.add(0,o.get(0));
-						l.add(1,o.get(1)+firstOutSize);
-						return l;
-
-					});
-					outStart++;
-				}
-				else {
-					long folderSize = folderSizes.get(dir).get(1);
+//				if(outStart ==0)
+//				{
+//					long firstOutSize = folderSizes.get(dir).get(0);
+//					folderSizes.merge(dir.getParent(),list,(o,n)->{
+//						List<Long> l = new ArrayList<>();
+//						l.add(0,o.get(0));
+//						l.add(1,o.get(1)+firstOutSize);
+//						return l;
+//
+//					});
+//					outStart++;
+//				}
+//				else {
+					long folderSize = folderSizes.get(dir).get(0);
+					long subFolderSize = folderSizes.get(dir).get(1);
 
 					folderSizes.merge(dir.getParent(), list, (o, n) ->
 
 					{
 						List<Long> l = new ArrayList<>();
-						l.add(0, o.get(0));
-						l.add(1, o.get(1) + folderSize);
+						l.add(0,0L);
+						l.add(1,0L);
+						l.set(0, o.get(0));
+						l.set(1, o.get(1) + folderSize + subFolderSize);
 						return l;
 
 					});
-					outStart++;
-				}
-////				subFolderSizes.merge(dir.getParent(),0L,(o,n)->o += folderSize);
+//					outStart++;
+//				}
+//				subFolderSizes.merge(dir.getParent(),0L,(o,n)->o += folderSize);
 
 			}
 
