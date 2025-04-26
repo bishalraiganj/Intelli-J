5b3ef86283ae619872b221cb6c4b8aabe89ba370
bishalraiganj <bishaladhikaryreal@gmail.com>
2025-04-26 15:55:13 +0530

    counting files, folders and sizes

--- FileWalker2/src/Adhikary/X/Main.java
+++ FileWalker2/src/Adhikary/X/Main.java
@@ -3,6 +3,7 @@ package Adhikary.X;
 import java.io.IOException;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Objects;
@@ -11,8 +12,8 @@ public class Main {
 
     public static void main(String... args) {
 
-        Path startingPath = Path.of("..");
-        FileVisitor<Path> statsVisitor = new StatsVisitor(1);
+        Path startingPath = Path.of(".");
+        FileVisitor<Path> statsVisitor = new StatsVisitor(Integer.MAX_VALUE);
         try {
             Files.walkFileTree(startingPath, statsVisitor);
         } catch (IOException e) {
@@ -20,13 +21,19 @@ public class Main {
         }
     }
 
-    private static class StatsVisitor extends SimpleFileVisitor<Path> {
+    private static class StatsVisitor implements FileVisitor<Path> {
 
         private Path initialPath = null;
-        private final Map<Path, Long> folderSizes = new LinkedHashMap<>();
+        private final Map<Path, Map<String,Long>> folderSizes = new LinkedHashMap<>();
+
+
         private int initialCount;
 
         private int printLevel;
+
+        private static final String DIR_CNT = "DirCount";
+        private static final String FILE_SIZE = "fileSize";
+        private static final String FILE_CNT = "fileCount";
 
         public StatsVisitor(int printLevel) {
             this.printLevel = printLevel;
@@ -38,8 +45,29 @@ public class Main {
 
             Objects.requireNonNull(file);
             Objects.requireNonNull(attrs);
-            folderSizes.merge(file.getParent(), 0L, (o, n) -> o += attrs.size());
+            Map<String,Long> parentMap = folderSizes.get(file.getParent());
+            if( parentMap != null)
+            {
+                long fileSize = attrs.size();
+                parentMap.merge(FILE_SIZE,fileSize,(o,n)->Math.addExact(o,n));
+                parentMap.merge(FILE_CNT,1L,(o,n)->Math.addExact(o,n));
+
+            }
             return FileVisitResult.CONTINUE;
+        }
+
+        @Override
+        public FileVisitResult visitFileFailed(Path file,IOException exc) throws IOException
+        {
+            Objects.requireNonNull(file);
+
+            if(exc!=null)
+            {
+                System.out.println(exc.getClass().getSimpleName() + " " + file);
+            }
+
+            return FileVisitResult.CONTINUE;
+
         }
 
         @Override
@@ -57,7 +85,7 @@ public class Main {
                 if (relativeLevel == 1) {
                     folderSizes.clear();
                 }
-                folderSizes.put(dir, 0L);
+                folderSizes.put(dir, new HashMap<>());
             }
             return FileVisitResult.CONTINUE;
         }
@@ -80,14 +108,29 @@ public class Main {
 
                     int level = key.getNameCount() - initialCount - 1;
                     if (level < printLevel) {
-                        System.out.printf("%s[%s] - %,d bytes %n",
-                                "\t".repeat(level), key.getFileName(), value);
+
+                        long size = value.getOrDefault(FILE_SIZE,0L);
+
+                        System.out.printf("%s[%s] - %,d bytes, %d files, %d folders %n","\t".repeat(level),key.getFileName(),size
+                                ,value.getOrDefault(FILE_CNT,0L)
+                                ,value.getOrDefault(DIR_CNT,0L));
+
                     }
                 });
 
             } else {
-                long folderSize = folderSizes.get(dir);
-                folderSizes.merge(dir.getParent(), 0L, (o, n) -> o += folderSize);
+
+                Map<String,Long> parentMap = folderSizes.get(dir.getParent());
+                Map<String,Long> childMap  = folderSizes.get(dir);
+                long folderCount = childMap.getOrDefault(DIR_CNT,0L);
+                long fileSize = childMap.getOrDefault(FILE_SIZE,0L);
+                long fileCount = childMap.getOrDefault(FILE_CNT,0L);
+
+                parentMap.merge(DIR_CNT,folderCount+1,(o,n)->o+=n);
+                parentMap.merge(FILE_SIZE,fileSize,(o,n)->Math.addExact(o,n));
+                parentMap.merge(FILE_CNT,fileCount,(o,n)->Math.addExact(o,n));
+
+
             }
             return FileVisitResult.CONTINUE;
         }
