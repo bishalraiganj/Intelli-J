1c53d523f9619b525aad751ce9c3aab408ddea0c
bishalraiganj <bishaladhikaryreal@gmail.com>
2025-04-27 22:54:52 +0530

    counting word frequencies

--- ReadingFilesChallenge/src/Adhikary/X/Main.java
+++ ReadingFilesChallenge/src/Adhikary/X/Main.java
@@ -4,7 +4,11 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Map;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
 
@@ -23,10 +27,26 @@ public class Main {
 //							.flatMap((s)-> Arrays.stream(s.split(pattern.toString())))
 //					.count());
 
+//
+//			System.out.printf("%,d words in file%n",br.lines()
+//					.mapToLong((e)->e.split(pattern.toString()).length)
+//					.sum());
 
-			System.out.printf("%,d words in file%n",br.lines()
-					.mapToLong((e)->e.split(pattern.toString()).length)
-					.sum());
+
+			System.out.println("-".repeat(50));
+
+			Map<String,Long> result = br.lines()
+					.flatMap((e)->pattern.splitAsStream(e))
+					.map((e)->e.replaceAll("\\p{Punct}+",""))
+					.map(String::toLowerCase)
+					.filter((e)->e.length()>4)
+					.collect(Collectors.groupingBy((e)->e,Collectors.counting()));
+
+			result.entrySet().stream()
+					.sorted(Comparator.comparing((e)->e.getValue(),Comparator.reverseOrder()))
+					.limit(10)
+					.forEach((e)->System.out
+					.println(e.getKey() + " - " + e.getValue() + "times"));
 
 		} catch(IOException e)
 		{
