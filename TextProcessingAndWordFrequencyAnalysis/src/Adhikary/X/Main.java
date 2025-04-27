1b8b1e69a140495506bd9fb1bd0d6433549ea089
bishalraiganj <bishaladhikaryreal@gmail.com>
2025-04-27 20:28:26 +0530

    reversed the order (Descending Order)  , collected into a LinkedHashMap first

--- TextProcessingAndWordFrequencyAnalysis/src/Adhikary/X/Main.java
+++ TextProcessingAndWordFrequencyAnalysis/src/Adhikary/X/Main.java
@@ -3,6 +3,7 @@ package Adhikary.X;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Comparator;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.regex.Pattern;
@@ -42,6 +43,28 @@ public class Main {
 					.collect(Collectors.groupingBy((e)->e,()->new LinkedHashMap<>(),Collectors.counting()));
 
 			wordFrequencies.entrySet().forEach(System.out::println);
+
+			System.out.println("-".repeat(50)+  " Here Ordered  based on the word frequencies  (Ascending Order ) !" + "-".repeat(50));
+			wordFrequencies.entrySet().stream()
+					.sorted(Comparator.comparing((entry)->entry.getValue()))
+					.forEach(System.out::println);
+
+
+
+
+
+			System.out.println("-".repeat(50) + "! Here , we have collected into a linkedHashMap and reversed the order (Descending Order) !");
+
+
+
+
+
+			wordFrequencies.entrySet()
+					.stream()
+					.sorted(Comparator.comparing((entry)->entry.getValue()))
+					.collect(()-> new LinkedHashMap<>(),(LinkedHashMap<String,Long> e1, Map.Entry<String,Long> e2)->e1.put(e2.getKey(),e2.getValue()),(e3,e4)->e3.putAll(e4))
+					.reversed()
+					.forEach((key,value)->System.out.println(key+ " = " + value));
 
 
 		} catch(IOException e)
