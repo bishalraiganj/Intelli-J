72343b258d100d64fd5ee45cbf9d2268b7190263
bishalraiganj <bishaladhikaryreal@gmail.com>
2025-05-02 15:50:34 +0530

    code ready Untested

---
+++ JSONDataFile/src/Adhikary/X/Main.java
@@ -0,0 +1,74 @@
+package Adhikary.X;
+
+import Adhikary.X.Student.Student;
+
+import java.util.StringJoiner;
+import java.util.stream.Stream;
+
+public class Main {
+
+	public static void main(String... args)
+	{
+
+		StringJoiner sj =  new StringJoiner("\n} \n , {\n ,","[{\n","\n}]");
+		Stream.generate(()-> Student.getRandomStudent())
+				.limit(1000)
+				.forEach((e)->{
+
+					StringJoiner engagementJoiner = new StringJoiner("\n}, { \n","[{\n","\n}]");
+
+
+					for(String record : e.getEngagementRecords() )
+					{
+						String[] parts = record.split(",");
+						String courseCode = parts[1];
+						String engagementType = parts[2];
+						String enrollmentMonth = parts[8];
+						String enrolledYear = parts[3];
+						String engagementString = """
+								courseCode : %s,
+								engagementType : %s,
+								enrollmentMonth : %s,
+								enrollmentYear : %s
+						""".formatted(courseCode,engagementType,
+								enrollmentMonth,enrolledYear);
+
+						engagementJoiner.add(engagementString);
+					}
+
+
+					String studentString = """
+							studentId : %d,
+							demographics : {
+								countryCode : %s,
+								enrollmentMonth : %d,
+								enrolledYear : %d,
+								ageAtEnrollment : %d,
+								gender : %s,
+								previousProgrammingExperience : %s
+								},
+								engagement : 
+							""".formatted(e.getStudentId()
+							,e.getCountry()
+							,e.getEnrollmentMonth()
+							,e.getEnrollmentYear()
+							,e.getEnrollmentAge()
+							,e.getGender()
+							,e.hasExperience()
+							,engagementJoiner.toString());
+
+
+					sj.add(studentString);
+
+				});
+
+
+
+
+
+
+
+
+	}
+
+}
