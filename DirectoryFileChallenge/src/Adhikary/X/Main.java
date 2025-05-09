c777c34e567beb54d5d0ced463c44929c6ee4f91
bishalraiganj <bishaladhikaryreal@gmail.com>
2025-05-09 20:06:40 +0530

    creating copies index.txt files

--- DirectoryFileChallenge/src/Adhikary/X/Main.java
+++ DirectoryFileChallenge/src/Adhikary/X/Main.java
@@ -3,6 +3,7 @@ package Adhikary.X;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 import java.nio.file.StandardOpenOption;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -23,6 +24,33 @@ public class Main {
 		{
 			System.out.println("Error Message: " + e.getMessage());
 		}
+		for(int i =1 ; i <= deepestFolder.getNameCount();i++)
+		{
+			Path indexedPath = deepestFolder.subpath(0,i).resolve("index.txt");
+			Path backupPath = deepestFolder.subpath(0,i).resolve("indexCopy.txt");
+			try
+			{
+				Files.copy(indexedPath,backupPath, StandardCopyOption.REPLACE_EXISTING);
+			}catch(IOException e)
+			{
+				System.out.println("Error Message: "+ e.getMessage());
+			}
+
+			try
+			{
+				generateIndexFile(deepestFolder.getName(0));
+
+			}catch(IOException e)
+			{
+				System.out.println("Error Message: " + e.getMessage());
+			}
+
+
+
+
+		}
+
+
 
 
 
