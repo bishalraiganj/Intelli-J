2190d60aaaf18113db1449652ba720d2372af7e4
bishalraiganj <bishaladhikaryreal@gmail.com>
2025-05-09 08:09:50 +0530

    logging all calculations done each time the program is being executed

--- FindFactors/src/Adhikary/X/Main.java
+++ FindFactors/src/Adhikary/X/Main.java
@@ -1,5 +1,9 @@
 package Adhikary.X;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -9,15 +13,15 @@ public class Main {
 	{
 
 
-		getFactCount(48);
+		System.out.println("Total number of factors : " + getFactCount(48));
 
 
 	}
 
 	private static int getFactCount(int n)
 	{
-		int prevFac = 0;
-		int currFac = 0;
+//		int prevFac = 0;
+//		int currFac = 0;
 
 		Map<Integer,Integer> factPairMap = new LinkedHashMap<>();
 
@@ -30,7 +34,38 @@ public class Main {
 		}
 		factPairMap.put(1,n);
 		factPairMap.forEach((k,v)->System.out.println(k + " - " + v));
-		return 0;
+
+
+		boolean perfectSquareCheck = (n % Math.sqrt(n) == 0)? true : false;
+		int numOfFac = (perfectSquareCheck) ? (factPairMap.size() * 2) - 1 : factPairMap.size() * 2;
+		return numOfFac;
+
+	}
+
+	private static void logCalculation(Map.Entry<Integer,Integer> entry)
+	{
+		if(!Files.exists(Path.of("calculationLog.txt")))
+		{
+			try {
+				Files.createDirectory(Path.of("calculationLog.txt"));
+			} catch(IOException e)
+			{
+				System.out.println("Error Message : " + e.getMessage());
+			}
+
+		}
+
+		try(FileWriter writer = new FileWriter("calculationLog.txt" , true))
+		{
+			writer.write(entry.getKey() + " - " + entry.getValue() + "\n");
+
+
+		} catch(IOException e)
+		{
+			System.out.println("Error Message : " + e.getMessage());
+		}
+
+
 	}
 
 }
