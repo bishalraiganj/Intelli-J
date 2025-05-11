8a6cc00997bcd4f5bb3c36f10609b4b4114d9a03
bishalraiganj <bishaladhikaryreal@gmail.com>
2025-05-11 10:57:20 +0530

    writing index and record data using RandomAccessFile in a .dat file

--- RandomAccessFile/src/Adhikary/X/Main.java
+++ RandomAccessFile/src/Adhikary/X/Main.java
@@ -1,7 +1,12 @@
 package Adhikary.X;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class Main {
 
+	private static final Map<Long,Long> indexedIds = new LinkedHashMap<>();
+	private static int recordsInFile = 0;
 	public static void main(String... args)
 	{
 
