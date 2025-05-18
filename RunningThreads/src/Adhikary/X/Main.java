a2ade8008e783edab61a7d7e83e7631ffc0a7e31
bishalraiganj <bishaladhikaryreal@gmail.com>
2025-05-18 19:28:45 +0530

    created a custom thread called 'thread' and running it concurrently /asynchronously with the main thread

---
+++ RunningThreads/src/Adhikary/X/Main.java
@@ -0,0 +1,59 @@
+package Adhikary.X;
+
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+
+	public static void main(String... args)
+	{
+		System.out.println("Main thread running");
+		try {
+			System.out.println("Main thread paused for one second");
+			Thread.sleep(1000);
+		} catch(InterruptedException e)
+		{
+			e.printStackTrace();
+		}
+
+		Thread thread = new Thread(()-> {
+
+			String tName = Thread.currentThread().getName();
+			System.out.println(tName + " should take 10 dots to run." );
+			for(int i = 0 ; i < 10 ; i++)
+			{
+				System.out.print(". ");
+				try{
+					Thread.sleep(500);
+				}catch(InterruptedException e)
+				{
+					System.out.println("\nWhoops!!" + tName + " interrupted");
+					return;
+				}
+			}
+			System.out.println("\n" + tName + "  thread execution completed");
+
+		});
+
+		System.out.println(thread.getName() + " thread starting");
+		thread.start();
+
+		System.out.println("Main thread would continue here");
+		while(thread.isAlive())
+		{
+			System.out.print(" M ");
+			try {
+				TimeUnit.MILLISECONDS.sleep(300);
+			}catch(InterruptedException e)
+			{
+				System.out.println("Main thread interrupted");
+			}
+		}
+		if(!thread.isAlive())
+		{
+			System.out.println(thread.getName() + " thread stopped" + " so stopping Main thread also :-)" );
+		}
+
+
+	}
+
+}
