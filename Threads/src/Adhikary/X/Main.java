776cf794630c20b3d27c662f16c8cfd08c46f255
bishalraiganj <bishaladhikaryreal@gmail.com>
2025-05-18 11:51:28 +0530

    Printing main threads attributes

---
+++ Threads/src/Adhikary/X/Main.java
@@ -0,0 +1,34 @@
+package Adhikary.X;
+
+public class Main {
+
+	public static void main(String... args)
+	{
+		Thread currentThread = Thread.currentThread();
+		System.out.println(currentThread.getClass().getName());
+
+		System.out.println(currentThread);
+		System.out.println(":".repeat(50));
+
+		printThreadState(currentThread);
+
+
+
+
+
+	}
+
+	public static void printThreadState(Thread thread)
+	{
+		System.out.println("-".repeat(50));
+		System.out.println("Thread ID: " + thread.getId() );
+		System.out.println("Thread Name: " + thread.getName());
+		System.out.println("Thread Priority: " + thread.getPriority());
+		System.out.println("Thread State: " + thread.getState());
+		System.out.println("Thread Group: " + thread.getThreadGroup());
+		System.out.println("Thread is Alive: " + thread.isAlive());
+		System.out.println("-".repeat(50));
+		System.out.println();
+	}
+
+}
