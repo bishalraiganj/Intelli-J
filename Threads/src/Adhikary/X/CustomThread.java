ba0ca4779bfd40f8ccf998ee79014de78cdd0ca0
bishalraiganj <bishaladhikaryreal@gmail.com>
2025-05-18 12:22:58 +0530

    added custom behavior to the custom Thread

--- Threads/src/Adhikary/X/CustomThread.java
+++ Threads/src/Adhikary/X/CustomThread.java
@@ -5,7 +5,18 @@ public class CustomThread extends Thread {
 	@Override
 	public void run()
 	{
-		super.run();
+//		super.run();
+		for(int i = 1; i <= 5 ; i++)
+		{
+			System.out.println(" 1 ");
+			try {
+				Thread.sleep(500); // Adding a 1-second delay between each count
+
+			}catch (InterruptedException e)
+			{
+				e.printStackTrace();
+			}
+		}
 	}
 
 
