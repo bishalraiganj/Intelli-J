b01dd5024b24b9bb09db056b73dc47cb392dcd03
bishalraiganj <bishaladhikaryreal@gmail.com>
2025-05-20 23:35:20 +0530

    logFileAnalyzer program involves File I/O , Stream pipelines, Regular Expressions

---
+++ LogFileAnalyzer/src/Adhikary/X/Main.java
@@ -0,0 +1,101 @@
+package Adhikary.X;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Pattern;
+
+public class Main {
+
+	public static void main(String... args)
+	{
+
+		String logData = """
+				[2025-05-18 09:23:45] INFO  - User john logged in from IP 192.168.0.10
+				[2025-05-18 09:24:10] ERROR - Invalid password attempt for user admin
+				[2025-05-18 09:24:30] WARN  - Disk space running low on server-01
+				[2025-05-18 09:25:01] INFO  - User alice uploaded file report.pdf
+				[2025-05-18 09:26:20] INFO  - User bob logged out
+				[2025-05-18 09:26:55] ERROR - User charlie attempted access to restricted area
+				[2025-05-18 09:27:15] DEBUG - Cache refreshed for user alice
+				[2025-05-18 09:28:03] INFO  - User david logged in from IP 192.168.0.22
+				[2025-05-18 09:29:11] ERROR - Failed to send email to user emily
+				[2025-05-18 09:30:42] WARN  - High memory usage detected
+				[2025-05-18 09:31:12] INFO  - User john downloaded monthly_report.xlsx
+				[2025-05-18 09:32:00] INFO  - Scheduled backup completed successfully
+				[2025-05-18 09:33:15] DEBUG - Session timeout check executed
+				[2025-05-18 09:34:21] ERROR - Uncaught exception in module payment
+				[2025-05-18 09:35:29] INFO  - User admin changed password
+				""";
+
+		Path p = Path.of("log.txt");
+		if(!Files.exists(p))
+		{
+			try {
+				Files.createFile(p);
+			}catch(IOException e)
+			{
+				e.printStackTrace();
+			}
+
+
+		}
+		writeLog(p, logData);
+
+		System.out.println(logData);
+		System.out.println(logReader(p));
+
+
+		System.out.println(":".repeat(50));
+		listLogs(logReader(p)).forEach(System.out::println);
+
+
+
+
+	}
+
+	private static void writeLog(Path path, String log)
+	{
+		try(BufferedWriter bw = new BufferedWriter(new FileWriter("log.txt")))
+		{
+			bw.write(log);
+
+		}catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+
+	}
+
+	private static String logReader(Path path)
+	{
+		String str = "";
+		try(BufferedInputStream bis = new BufferedInputStream(new FileInputStream(path.toFile())))
+		{
+
+		byte[] bArr = bis.readAllBytes();
+		str = new String(bArr, StandardCharsets.UTF_8);
+
+
+		}catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+		return str;
+	}
+
+	private static List<String> listLogs(String logString)
+	{
+		String[] logs = logString.split("\n");
+
+		List<String> list = new ArrayList<>(Arrays.asList(logs));
+
+		return list;
+
+	}
+
+}
