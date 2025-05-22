3a5b979d818418054984354575dd9f3b780b1aae
bishalraiganj <bishaladhikaryreal@gmail.com>
2025-05-22 12:53:32 +0530

    added comments

--- MultiThreadedProgram/src/Adhikary/X/Main.java
+++ MultiThreadedProgram/src/Adhikary/X/Main.java
@@ -54,6 +54,14 @@ public class Main {
 
 		});
 
+
+
+		/*
+		threadThree (Thread instance) prints primeNumbers it's prime logic is optimized by only checking odd numbers that are not multiples of 2 and 3
+		and so the numbers are also checked for divisibility only against odd numbers that are also not 2 and 3's multiples
+		because we already know i's in the for loop are not so we dont have to check i's divisibility also by 2 and 3's multiples
+		but only by odd numbers that are not 2 and 3's multiples "
+		 */
 		Thread threadThree = new Thread(new Thread() {
 
 			@Override
@@ -63,10 +71,10 @@ public class Main {
 
 				for(int i = 3 ; primeCount < 500 ; i += 2)
 				{
-					if(i % 2 != 0 && i % 3 != 0)
+					if(i % 2 != 0 && i % 3 != 0) // This condition is unnecessary since we are already checking only odd numbers that are not multiples of either 2 and 3
 					{
 						boolean forLoopBreakFlag = false;
-						for(int j = 3 ; j <= Math.abs(Math.sqrt(i)) ; j += 2)
+						for(int j = 3 ; j <= Math.abs(Math.sqrt(i)) ; j += 2) // Here , we are only checking divisibility only by odd numbers that are not multiples of 2 and 3 for further optimization
 						{
 							if(i % j == 0)
 							{
