f00904e91faabc3805e89ce08254b61a0f715dd6
bishalraiganj <bishaladhikaryreal@gmail.com>
2025-05-24 00:59:02 +0530

    Because of cpu caching of the flag shared var/value the updates are not immediately visible to other threads immediately for the performance optimizations maintained by the jvm thus creating memory consistency error

--- MultipleThreads/src/Adhikary/X/Main.java
+++ MultipleThreads/src/Adhikary/X/Main.java
@@ -7,14 +7,16 @@ public class Main {
 	public static void main(String... args)
 	{
 
-		StopWatch stopWatch = new StopWatch(TimeUnit.SECONDS);
+		StopWatch greenWatch = new StopWatch(TimeUnit.SECONDS);
+		StopWatch purpleWatch = new StopWatch(TimeUnit.SECONDS);
+		StopWatch redWatch = new StopWatch(TimeUnit.SECONDS);
 		Thread green = new Thread(()->{
-			stopWatch.countDown();
+			greenWatch.countDown();
 		},ThreadColor.ANSI_GREEN.name());
 
-		Thread purple = new Thread(stopWatch::countDown,ThreadColor.ANSI_PURPLE.name());
+		Thread purple = new Thread(purpleWatch::countDown,ThreadColor.ANSI_PURPLE.name());
 		Thread red = new Thread(()-> {
-			stopWatch.countDown(9);
+			redWatch.countDown(9);
 		},ThreadColor.ANSI_RED.name());
 
 		green.start();
