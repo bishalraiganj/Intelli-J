1fc9d8cc2ba623ef016e39f31377ff16521ef5e2
bishalraiganj <bishaladhikaryreal@gmail.com>
2025-05-24 13:07:18 +0530

    memory inconsistency and thread interference( because of interleaving intermediate states are observed by other threads ) causing inconsistency

---
+++ Synchronization/src/Adhikary/X/Main.java
@@ -0,0 +1,32 @@
+package Adhikary.X;
+
+public class Main {
+
+	public static void main(String... args)
+	{
+
+		BankAccount companyAccount = new BankAccount();
+
+		Thread thread1 = new Thread(()-> companyAccount.withdraw(2500));
+		Thread thread2 = new Thread(()-> companyAccount.deposit(5000));
+		Thread thread3 = new Thread(()-> companyAccount.withdraw(5000));
+
+		thread1.start();
+		thread2.start();
+		thread3.start();
+
+		try{
+			thread1.join();
+			thread2.join();
+			thread3.join();
+		}catch(InterruptedException e)
+		{
+			e.printStackTrace();
+		}
+
+		System.out.println("Final Balance: " + companyAccount.getBalance());
+
+	}
+
+
+}
