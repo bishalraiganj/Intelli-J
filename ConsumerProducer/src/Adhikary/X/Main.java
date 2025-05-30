4450d7d0e3642669dc8c6ea3cfc0bd6b61a3045b
bishalraiganj <bishaladhikaryreal@gmail.com>
2025-05-31 01:42:59 +0530

    produced a deadlock situtation because of a shared resource with multiple synchronized methods used by multiple threads

--- ConsumerProducer/src/Adhikary/X/Main.java
+++ ConsumerProducer/src/Adhikary/X/Main.java
@@ -107,8 +107,13 @@ public class Main {
 
 	public static void main(String... args)
 	{
+		MessageRepository messageRepository = new MessageRepository();
 
+		Thread reader = new Thread(new MessageReader(messageRepository));
+		Thread writer = new Thread(new MessageWriter(messageRepository));
 
+		reader.start();
+		writer.start();
 
 
 
