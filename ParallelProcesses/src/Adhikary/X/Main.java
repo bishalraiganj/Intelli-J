d94698044ffcc8ac3a224babf52916a3d32bf15d
Bishal Adhikary <158996175+bishalraiganj@users.noreply.github.com>
2025-06-17 01:08:32 +0530

    workStealingPool parallelism level/cpu count 4 and 10 callable tasks

---
+++ ParallelProcesses/src/Adhikary/X/Main.java
@@ -0,0 +1,64 @@
+package Adhikary.X;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class Main {
+
+	public static void main(String... args) throws Exception
+	{
+		int numbersLength = 100_000;
+		long[] numbers =  new Random().longs(numbersLength,1,
+				numbersLength)
+				.toArray();
+
+		long sum = Arrays.stream(numbers)
+				.reduce(0L,(a,b)->a+b);
+		System.out.println(sum);
+
+
+		ExecutorService threadPool = Executors.newWorkStealingPool(4);
+		List<Callable<Long>> tasks = new ArrayList<>();
+
+		int taskNo = 10 ;
+
+		int splitCount = numbersLength / taskNo;
+
+		for( int i = 0 ; i < taskNo ; i++)
+		{
+			int start = i * splitCount ;
+			int end = start + splitCount;
+			tasks.add(()->{
+				long tasksum = 0;
+				for(int j = start ; j < end ; j++)
+				{
+					tasksum += numbers[j];
+				}
+				return tasksum;
+			});
+		}
+
+		List<Future<Long>> futures = threadPool.invokeAll(tasks);
+
+		long tasksum = 0;
+
+		for(Future<Long> future : futures)
+		{
+			tasksum += future.get();
+		}
+
+		System.out.println("Thread Pool Sum = " + tasksum);
+		threadPool.shutdown();
+
+		System.out.println(threadPool.getClass().getName());
+
+	}
+
+
+}
