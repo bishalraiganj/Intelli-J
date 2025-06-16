c6c03f7cd7db696f85182587992151ec1ff2119e
Bishal Adhikary <158996175+bishalraiganj@users.noreply.github.com>
2025-06-17 04:21:45 +0530

    ResursiveTask with threshold by task count not by smallest task work amount ,  ( the threshold is divison field which determines the workamount when the array numbers divided by divison because divison represent the total number of tasks possible )

--- ParallelProcesses/src/Adhikary/X/Main.java
+++ ParallelProcesses/src/Adhikary/X/Main.java
@@ -6,6 +6,51 @@ import java.util.List;
 import java.util.Random;
 import java.util.concurrent.*;
 
+
+class RecursiveSumTask extends RecursiveTask<Long> {
+
+
+	private final long[] numbers;
+
+	private final int start;
+
+	private final int end;
+
+	private final int divison;
+
+	public RecursiveSumTask(long[] numbers, int start , int end , int division)
+	{
+		this.numbers = numbers;
+		this.start  = start;
+		this.end = end;
+		this.divison = division;
+
+	}
+	@Override
+	protected Long compute()
+	{
+		if((end - start )  <= ( numbers.length / divison ) )
+		{
+
+			System.out.println(start + " : " + end);
+			long sum = 0 ;
+			for(int i = start ; i < end ; i++)
+			{
+				sum += numbers[i];
+			}
+			return sum;
+		}
+		else {
+			int mid = ( start + end ) / 2 ;
+			RecursiveSumTask left = new RecursiveSumTask(numbers,start,mid,divison);
+			RecursiveSumTask right = new RecursiveSumTask(numbers,mid,end,divison);
+			left.fork();
+			right.fork();
+			return left.join() + right.join();
+		}
+	}
+}
+
 public class Main {
 
 	public static void main(String... args) throws Exception
@@ -20,7 +65,7 @@ public class Main {
 		System.out.println(sum);
 
 
-		ForkJoinPool threadPool =(ForkJoinPool) Executors.newWorkStealingPool(4);
+		ForkJoinPool threadPool = ForkJoinPool.commonPool();
 		List<Callable<Long>> tasks = new ArrayList<>();
 
 		int taskNo = 10 ;
@@ -43,6 +88,7 @@ public class Main {
 
 		List<Future<Long>> futures = threadPool.invokeAll(tasks);
 
+		System.out.println("CPU's : " + Runtime.getRuntime().availableProcessors());
 		System.out.println("Parallelism = " + threadPool.getParallelism());
 		System.out.println("Pool size = " + threadPool.getPoolSize());
 		System.out.println("Steal count = " + threadPool.getStealCount());
@@ -55,6 +101,10 @@ public class Main {
 		}
 
 		System.out.println("Thread Pool Sum = " + tasksum);
+
+		RecursiveTask<Long> task = new RecursiveSumTask(numbers,0,numbersLength,8);
+		long forkJoinSum = threadPool.invoke(task);
+		System.out.println("Recursive sum is: " + forkJoinSum);
 		threadPool.shutdown();
 
 		System.out.println(threadPool.getClass().getName());
