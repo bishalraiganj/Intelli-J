0639a1c858687446d72af4f55e369633a6ad5eb6
Bishal Adhikary <158996175+bishalraiganj@users.noreply.github.com>
2025-06-16 15:53:54 +0530

    newSingleThreadScheduledExecutor with 2 sec

--- SchedulingTasks/src/Adhikary/X/Main.java
+++ SchedulingTasks/src/Adhikary/X/Main.java
@@ -9,55 +9,55 @@ import java.util.concurrent.*;
 
 public class Main {
 
-	public static void main(String... args)
-	{
+	public static void main(String... args) {
 		DateTimeFormatter dtf = DateTimeFormatter.ofLocalizedDateTime(
 				FormatStyle.MEDIUM,
 				FormatStyle.LONG
 		);
 
-		Callable<ZonedDateTime> waitThenDoIt = ()->
+		Callable<ZonedDateTime> waitThenDoIt = () ->
 		{
 			ZonedDateTime zdt = null;
-			try
-			{
+			try {
 				TimeUnit.SECONDS.sleep(2);
 				zdt = ZonedDateTime.now();
-			}catch(InterruptedException  e)
-			{
+			} catch (InterruptedException e) {
 				throw new RuntimeException(e);
 			}
 			return zdt;
 		};
 
 
-		ExecutorService threadPool = Executors.newFixedThreadPool(4);
-		List<Callable<ZonedDateTime>> list = Collections.nCopies(4,waitThenDoIt);
+		ExecutorService threadPool = Executors.newFixedThreadPool(2);
+		List<Callable<ZonedDateTime>> list = Collections.nCopies(4, waitThenDoIt);
 
-		try
-		{
+		try {
 			System.out.println("---> " + ZonedDateTime.now().format(dtf));
 			List<Future<ZonedDateTime>> futureList = threadPool.invokeAll(list);
-			for(Future<ZonedDateTime> result : futureList)
-			{
-				try
-				{
-					System.out.println(result.get(1,TimeUnit.SECONDS).format(dtf));
-				}catch(Exception e)
-				{
+			for (Future<ZonedDateTime> result : futureList) {
+				try {
+					System.out.println(result.get(1, TimeUnit.SECONDS).format(dtf));
+				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
-		}catch(InterruptedException  e)
-		{
+		} catch (InterruptedException e) {
 			throw new RuntimeException(e);
-		}
-		finally
-		{
+		} finally {
 			threadPool.shutdown();
 		}
 
+		System.out.println("---> " + ZonedDateTime.now().format(dtf));
+		ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
+
+		executor.schedule(() ->
+		{
+			System.out.println(ZonedDateTime.now().format(dtf));
+
+
+		}, 2, TimeUnit.SECONDS);
+
+		executor.shutdown();
 
 	}
-
 }
