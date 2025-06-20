ed2b932c9ffa992fb2a447fb085f29c965cf6025
Bishal Adhikary <158996175+bishalraiganj@users.noreply.github.com>
2025-06-20 16:03:24 +0530

    checking the  average of difference  between elapsedSerial and parallelSerial

--- untitled/src/Adhikary/X/Main.java
+++ untitled/src/Adhikary/X/Main.java
@@ -5,33 +5,38 @@ import java.util.Random;
 
 public class Main {
 
-	public static void main(String... args)
-	{
+	public static void main(String... args) {
 		int numbersLength = 100_000;
-		long[] numbers = new Random().longs(numbersLength,1,numbersLength).toArray();
+		long[] numbers = new Random().longs(numbersLength, 1, numbersLength).toArray();
 
 
-		long start = System.nanoTime();
-		double averageSerial = Arrays.stream(numbers)
-				.average()
-				.orElseThrow();
+		long delta = 0;
+		int iterations = 100;
 
-		long elapsedSerial = System.nanoTime() - start;
-		System.out.printf("Ave = %.2f , elapsed = %d nano or %.2f ms%n", averageSerial,elapsedSerial,
-				elapsedSerial / 1000000.0);
-
-
-
+		for (int i = 0; i < iterations; i++) {
+			long start = System.nanoTime();
+			double averageSerial = Arrays.stream(numbers)
+					.average()
+					.orElseThrow();
+			long elapsedSerial = System.nanoTime() - start;
 
 
-		start = System.nanoTime();
-		double averageParallel = Arrays.stream(numbers)
-				.parallel()
-				.average()
-				.orElseThrow();
-		long elapsedParallel = System.nanoTime() - start;
-		System.out.printf("Ave = %.2f , elapsed = %d nanos or %.2f ms%n",averageParallel,elapsedParallel,elapsedParallel/1000000.0);
+
+			start = System.nanoTime();
+			double averageParallel = Arrays.stream(numbers)
+					.parallel()
+					.average()
+					.orElseThrow();
+			long elapsedParallel = System.nanoTime() - start;
+			delta += (elapsedSerial - elapsedParallel);
+
+		}
+
+		System.out.printf("Parallel is [%d] nanos or [%.2f] ms faster on average %n",
+				delta/iterations, delta / 1000000.00/iterations);
 	}
+
+
 
 
 }
